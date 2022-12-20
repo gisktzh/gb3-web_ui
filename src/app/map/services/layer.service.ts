05bffe79803b262c5710cd2f5b9903e0686d376a
Lukas Merz <lukemerz@gmail.com>
2022-12-20 16:50:35 +0100

    GB3-311: Remove layerconfig

--- src/app/map/services/layer.service.ts
+++ src/app/map/services/layer.service.ts
@@ -1,7 +1,5 @@
 import {Injectable} from '@angular/core';
 import {BehaviorSubject, Observable} from 'rxjs';
-import {LayersConfig} from '../../../assets/layers.config';
-import {EsriGroupLayer, EsriWMSLayer} from '../../shared/external/esri.module';
 import {MapService} from './map.service';
 
 @Injectable({
@@ -9,14 +7,6 @@ import {MapService} from './map.service';
 })
 export class LayerService {
   private readonly activeLayers$: BehaviorSubject<Array<__esri.Layer>> = new BehaviorSubject<Array<__esri.Layer>>([]);
-
-  public get activeLayers(): Observable<Array<__esri.Layer>> {
-    return this.activeLayers$.asObservable();
-  }
-
-  public get currentActiveLayers(): __esri.Layer[] {
-    return this.activeLayers$.value;
-  }
 
   constructor(private readonly mapService: MapService) {
     this.initLayers();
@@ -26,21 +16,15 @@ export class LayerService {
     });
   }
 
+  public get activeLayers(): Observable<Array<__esri.Layer>> {
+    return this.activeLayers$.asObservable();
+  }
+
+  public get currentActiveLayers(): __esri.Layer[] {
+    return this.activeLayers$.value;
+  }
+
   private initLayers(): void {
-    const groupLayers: __esri.GroupLayer[] = [];
-    LayersConfig.forEach((layerConfig) => {
-      const uniqueLayers: __esri.Layer[] = [];
-      layerConfig.layer.split(',').map((sublayer) => {
-        const layer = new EsriWMSLayer({
-          id: layerConfig.id,
-          url: layerConfig.url,
-          sublayers: [{name: sublayer, title: sublayer}],
-          description: layerConfig.queryLayerName
-        });
-        uniqueLayers.push(layer);
-      });
-      groupLayers.push(new EsriGroupLayer({layers: uniqueLayers, id: layerConfig.name, title: layerConfig.queryLayerName}));
-    });
-    this.activeLayers$.next(groupLayers);
+    this.activeLayers$.next([]);
   }
 }
