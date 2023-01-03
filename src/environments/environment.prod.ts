0eeab4a720797f50d8bf5fc2571df1bb396855fb
Lukas Merz <lukemerz@gmail.com>
2023-01-03 16:52:36 +0100

    GB3-406: Add basic Dockerfile, adjust production configuration settings to also use the API key for GB2 Backend

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,8 @@
+import {apiKey} from './environment.local';
+
 export const environment = {
   production: true,
+  apiKey: apiKey,
   baseUrls: {
     gb3Api: 'https://maps.zh.ch',
     geoLion: 'https://www.geolion.zh.ch'
--- src/main.ts
+++ src/main.ts
@@ -7,18 +7,18 @@ import esriConfig from '@arcgis/core/config';
 
 if (environment.production) {
   enableProdMode();
-} else {
-  /**
-   * Required configuration to append user_token to each request. This is used as a workaround only because we're
-   * currently required to use the api key to access GB2 backend.
-   */
-  esriConfig.request.interceptors?.push({
-    urls: ['https://maps.zh.ch', 'http://wms.zh.ch'],
-    query: {
-      USER_TOKEN: environment.apiKey
-    }
-  });
 }
+
+/**
+ * Required configuration to append user_token to each request. This is used as a workaround only because we're
+ * currently required to use the api key to access GB2 backend.
+ */
+esriConfig.request.interceptors?.push({
+  urls: ['https://maps.zh.ch', 'http://wms.zh.ch'],
+  query: {
+    USER_TOKEN: environment.apiKey
+  }
+});
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
