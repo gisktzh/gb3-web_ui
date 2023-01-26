a44ee602efc4dddfdf59e2d457bebd5e7e2e4c7e
Lukas Merz <lukemerz@gmail.com>
2023-01-26 17:07:14 +0100

    GB3-316: Fix tests

--- src/app/map/services/basemap-configuration.service.spec.ts
+++ src/app/map/services/basemap-configuration.service.spec.ts
@@ -5,9 +5,9 @@ import {Basemap} from '../../shared/interfaces/background-map.interface';
 import {ConfigService} from '../../shared/services/config.service';
 
 const mockBasemaps: Basemap[] = [
-  {id: 'test-1', url: 'https://www.my-test.com/test1', title: 'Test 1', srsId: 2056, layers: []},
-  {id: 'test-2', url: 'https://www.my-test.com/test2', title: 'Test 2', srsId: 2056, layers: []},
-  {id: 'test-3', url: 'https://www.my-test.com/test3', title: 'Test 3', srsId: 2056, layers: []}
+  {id: 'test-1', relativeImagePath: 'test-1.png', url: 'https://www.my-test.com/test1', title: 'Test 1', srsId: 2056, layers: []},
+  {id: 'test-2', relativeImagePath: 'test-2.png', url: 'https://www.my-test.com/test2', title: 'Test 2', srsId: 2056, layers: []},
+  {id: 'test-3', relativeImagePath: 'test-3.png', url: 'https://www.my-test.com/test3', title: 'Test 3', srsId: 2056, layers: []}
 ];
 
 const mockBasemapConfig = {
--- src/app/shared/pipes/background-map-image-link.pipe.spec.ts
+++ src/app/shared/pipes/background-map-image-link.pipe.spec.ts
@@ -1,13 +1,51 @@
 import {BasemapImageLinkPipe} from './background-map-image-link.pipe';
+import {ConfigService} from '../services/config.service';
+import {TestBed} from '@angular/core/testing';
+import {Basemap} from '../interfaces/background-map.interface';
+
+const mockBasemaps: Basemap[] = [
+  {id: 'test-1', relativeImagePath: 'path/to/image.png', url: 'https://www.my-test.com/test1', title: 'Test 1', srsId: 2056, layers: []}
+];
+
+const mockBasemapConfig = {
+  availableBasemaps: mockBasemaps,
+  defaultBasemap: mockBasemaps[0]
+};
 
 describe('BasemapImageLinkPipe', () => {
-  it('returns the correct filename', () => {
-    const pipe = new BasemapImageLinkPipe();
-    const testIdentifier = 'my-test-image';
+  let pipe: BasemapImageLinkPipe;
+  let configServiceSpy: jasmine.SpyObj<ConfigService>;
+  beforeEach(() => {
+    const spy = jasmine.createSpyObj('ConfigService', [], {basemapConfig: mockBasemapConfig});
 
-    const result = pipe.transform('my-test-image');
+    TestBed.configureTestingModule({
+      providers: [
+        BasemapImageLinkPipe,
+        {
+          provide: ConfigService,
+          useValue: spy
+        }
+      ]
+    });
+    pipe = TestBed.inject(BasemapImageLinkPipe);
+    configServiceSpy = TestBed.inject(ConfigService) as jasmine.SpyObj<ConfigService>;
+  });
 
-    const expected = `assets/images/basemaps/${testIdentifier}.png`;
+  it('returns the correct  for existing id', () => {
+    const testIdentifier = mockBasemaps[0].id;
+
+    const result = pipe.transform(testIdentifier);
+
+    const expected = mockBasemaps[0].relativeImagePath;
+    expect(result).toEqual(expected);
+  });
+
+  it('returns the empty string if id does not exist', () => {
+    const testIdentifier = 'does-not-exist';
+
+    const result = pipe.transform(testIdentifier);
+
+    const expected = '';
     expect(result).toEqual(expected);
   });
 });
