244e6429ab2469878b5ed6cd05939c795c08b35a
Lukas Merz <lukemerz@gmail.com>
2023-01-26 13:57:08 +0100

    Fix broken http links in an https environment with wms.zh.ch

--- src/main.ts
+++ src/main.ts
@@ -14,11 +14,21 @@ if (environment.production) {
  * currently required to use the api key to access GB2 backend.
  */
 esriConfig.request.interceptors?.push({
-  urls: ['https://maps.zh.ch', 'http://wms.zh.ch'],
+  urls: ['https://maps.zh.ch', 'https://wms.zh.ch'],
   query: {
     USER_TOKEN: environment.apiKey
   }
 });
+
+/**
+ * Because the GetCapabalities response often sends a non-secure http://wms.zh.ch response, Esri Javascript API fails on https environments
+ * to attach the token above if the urls is set to http://wms.zh.ch; because it automatically upgrades insecure links to https to avoid
+ * mixed-content.
+ *
+ * In order for it to still work on localhost, the above interceptor only listens to https, and the following setting tells our Esri map
+ * that requests to wms.zh.ch are always okay to be upgraded to https - even in localhost environments.
+ */
+esriConfig.request.httpsDomains?.push('wms.zh.ch');
 
 platformBrowserDynamic()
   .bootstrapModule(AppModule)
