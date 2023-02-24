3fcd823d0def5d76179ab5b198f7a22172515001
Lukas Merz <lukemerz@gmail.com>
2023-02-24 10:53:25 +0100

    GB3-302: Add progress indication

--- src/app/map/onboarding-guide/onboarding-guide/onboarding-guide.component.ts
+++ src/app/map/onboarding-guide/onboarding-guide/onboarding-guide.component.ts
@@ -1,11 +1,38 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy} from '@angular/core';
 import {TourService} from 'ngx-ui-tour-md-menu';
+import {IMdStepOption} from 'ngx-ui-tour-md-menu/lib/step-option.interface';
+import {Subscription, tap} from 'rxjs';
 
 @Component({
   selector: 'onboarding-guide',
   templateUrl: './onboarding-guide.component.html',
   styleUrls: ['./onboarding-guide.component.scss']
 })
-export class OnboardingGuideComponent {
-  constructor(public readonly tourService: TourService) {}
+export class OnboardingGuideComponent implements OnDestroy {
+  public progress: number = 0;
+  private readonly numberOfSteps: number = this.tourService.steps.length;
+  private readonly subscriptions: Subscription = new Subscription();
+
+  constructor(public readonly tourService: TourService) {
+    this.initSubscriptions();
+  }
+
+  public ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
+
+  private initSubscriptions() {
+    this.subscriptions.add(
+      this.tourService.stepShow$
+        .pipe(
+          tap(({step}) => {
+            const stepIndex = this.tourService.steps.indexOf(step);
+            const currentStepNumber = stepIndex + 1;
+
+            this.progress = (currentStepNumber / this.numberOfSteps) * 100;
+          })
+        )
+        .subscribe()
+    );
+  }
 }
