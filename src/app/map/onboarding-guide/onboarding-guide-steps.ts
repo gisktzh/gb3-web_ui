86896971d0bc826ca5df844ed453f3098b124d1e
Lukas Merz <lukemerz@gmail.com>
2023-02-24 11:23:09 +0100

    Add final step

--- src/app/map/onboarding-guide/onboarding-guide-steps.ts
+++ src/app/map/onboarding-guide/onboarding-guide-steps.ts
@@ -4,7 +4,7 @@ import {IMdStepOption} from 'ngx-ui-tour-md-menu/lib/step-option.interface';
  * All TourAnchors that are in use throughout the app. Do not use string anchors or tourAnchor directly, but add new steps and their anchors
  * to this type.
  */
-export type OnboardingGuideAnchor = 'start.tour' | 'map.catalogue' | 'active.map.items' | 'basemap.selector' | 'feature.info';
+export type OnboardingGuideAnchor = 'start.tour' | 'map.catalogue' | 'active.map.items' | 'basemap.selector' | 'feature.info' | 'end.tour';
 
 /**
  * Ovveride of the default IMdStepOption to force the usage of the OnboardingGuideAnchor types, which are also used in the TypedTourAnchor
@@ -43,6 +43,12 @@ export const onboardingGuideSteps: OnboardingStep[] = [
     anchorId: 'feature.info',
     content: 'Klicken Sie auf Kartenobjekte und erhalten Sie Informationen dazu.',
     title: 'Info-Klick',
+    enableBackdrop: false
+  },
+  {
+    anchorId: 'end.tour',
+    content: 'Wir wünschen viel Vergnügen bei der Nutzung des GIS-Browsers!',
+    title: 'Das wars!',
     enableBackdrop: false
   }
 ];
