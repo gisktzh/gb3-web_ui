c1d9f3b473fd57b312f10b6a71c8ff9dca08f701
Lukas Merz <lukemerz@gmail.com>
2023-04-03 16:36:18 +0200

    Added FAQ

--- src/app/state/support/reducers/support-content.reducer.ts
+++ src/app/state/support/reducers/support-content.reducer.ts
@@ -1,14 +1,18 @@
 import {createFeature, createReducer} from '@ngrx/store';
 import {supportLinks} from '../../../shared/models/support-links.data';
 import {SupportLinkCollection} from '../../../shared/interfaces/support-link.interface';
+import {FaqCollection} from '../../../shared/interfaces/faq.interface';
+import {faqData} from '../../../shared/models/faq.data';
 
 export const supportContentFeatureKey = 'supportContent';
 
 export interface SupportContentState {
+  faq: FaqCollection[];
   links: SupportLinkCollection[];
 }
 
 export const initialState: SupportContentState = {
+  faq: faqData,
   links: supportLinks
 };
 
@@ -17,4 +21,4 @@ export const supportContentFeature = createFeature({
   reducer: createReducer(initialState)
 });
 
-export const {name, reducer, selectSupportContentState, selectLinks} = supportContentFeature;
+export const {name, reducer, selectLinks, selectFaq} = supportContentFeature;
--- src/app/support-page/components/faq/faq.component.ts
+++ src/app/support-page/components/faq/faq.component.ts
@@ -1,8 +1,38 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Observable, Subscription, tap} from 'rxjs';
+import {selectFaq} from '../../../state/support/reducers/support-content.reducer';
+import {Store} from '@ngrx/store';
+import {FaqCollection} from '../../../shared/interfaces/faq.interface';
 
 @Component({
   selector: 'app-faq',
   templateUrl: './faq.component.html',
   styleUrls: ['./faq.component.scss']
 })
-export class FaqComponent {}
+export class FaqComponent implements OnInit, OnDestroy {
+  public faqCollections: FaqCollection[] = [];
+  private readonly faqCollections$: Observable<FaqCollection[]> = this.store.select(selectFaq);
+  private readonly subscriptions: Subscription = new Subscription();
+
+  constructor(private readonly store: Store) {}
+
+  public ngOnDestroy() {
+    this.subscriptions.unsubscribe();
+  }
+
+  public ngOnInit() {
+    this.initSubscriptions();
+  }
+
+  private initSubscriptions() {
+    this.subscriptions.add(
+      this.faqCollections$
+        .pipe(
+          tap((faqCollections) => {
+            this.faqCollections = faqCollections;
+          })
+        )
+        .subscribe()
+    );
+  }
+}
