6fee28be12fbce3b564a2cf9c3ed4e606164d892
Sebastian Wendland <Sebastian.Wendland@ebp.ch>
2023-04-04 17:37:54 +0200

    Moved `link-list.component` from the module `shared` to `kt-zh-design-system` and renamed it to `ktzh-link-list.component`.

--- src/app/kt-zh-design-system/kt-zh-design-system.module.ts
+++ src/app/kt-zh-design-system/kt-zh-design-system.module.ts
@@ -2,10 +2,11 @@ import {NgModule} from '@angular/core';
 import {SharedModule} from '../shared/shared.module';
 import {KtzhSliderWrapperComponent} from './components/ktzh-slider-wrapper/ktzh-slider-wrapper.component';
 import {CommonModule} from '@angular/common';
+import {KtzhLinkListComponent} from './components/ktzh-link-list/ktzh-link-list.component';
 
 @NgModule({
-  declarations: [KtzhSliderWrapperComponent],
+  declarations: [KtzhSliderWrapperComponent, KtzhLinkListComponent],
   imports: [CommonModule, SharedModule],
-  exports: [KtzhSliderWrapperComponent]
+  exports: [KtzhSliderWrapperComponent, KtzhLinkListComponent]
 })
 export class KtZhDesignSystemModule {}
