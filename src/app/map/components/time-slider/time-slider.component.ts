95067a6132ed9dd73e29cdca420337bb1f2d8b56
Sebastian Wendland <Sebastian.Wendland@ebp.ch>
2023-04-05 16:00:43 +0200

    GB3-400: Fixed time-slider.component by showing the simple value (e.g. '2001' instead of '2001-2002') only in case of simple ranges (single unit, one step).

--- src/app/map/components/time-slider/time-slider.component.ts
+++ src/app/map/components/time-slider/time-slider.component.ts
@@ -3,7 +3,12 @@ import {Store} from '@ngrx/store';
 import {TimeExtent} from '../../interfaces/time-extent.interface';
 import {TimeSliderConfiguration} from '../../../shared/interfaces/topic.interface';
 import * as dayjs from 'dayjs';
+import {ManipulateType} from 'dayjs';
 import {TimeSliderService} from '../../services/time-slider.service';
+import {TimeExtentUtil} from '../../../shared/utils/time-extent.util';
+import * as duration from 'dayjs/plugin/duration';
+
+dayjs.extend(duration);
 
 @Component({
   selector: 'time-slider',
@@ -27,6 +32,8 @@ export class TimeSliderComponent implements OnInit {
 
   private availableDates!: Date[];
   private timeExtent!: TimeExtent;
+  // the time slider shows a simple current value (e.g. `2001` instead of `2001-2002`) if it has a range of exactly one of a single time unit (year, month, ...)
+  private hasSimpleCurrentValue: boolean = false;
 
   constructor(private readonly store: Store, private readonly timeSliderService: TimeSliderService) {}
 
@@ -36,9 +43,10 @@ export class TimeSliderComponent implements OnInit {
     this.timeExtent = {start: this.initialTimeExtent.start, end: this.initialTimeExtent.end};
     this.firstSliderPosition = this.findPositionOfDate(this.timeExtent.start) ?? 0;
     this.secondSliderPosition = this.timeSliderConfiguration.range ? undefined : this.findPositionOfDate(this.timeExtent.end);
-    this.formattedTimeExtent = this.convertTimeExtentToString(this.timeExtent);
     this.effectiveFormattedMinimumValue = this.convertDateToString(this.availableDates[0]);
     this.effectiveFormattedMaximumValue = this.convertDateToString(this.availableDates[this.numberOfAvailableDates - 1]);
+    this.hasSimpleCurrentValue = this.isRangeExactlyOneOfSingleTimeUnit(this.timeSliderConfiguration.range);
+    this.formattedTimeExtent = this.convertTimeExtentToString(this.timeExtent);
   }
 
   public refreshCurrentValue() {
@@ -90,6 +98,31 @@ export class TimeSliderComponent implements OnInit {
     this.formattedTimeExtent = this.convertTimeExtentToString(this.timeExtent);
   }
 
+
+  /**
+   * Returns `true` if the given range is defined and is exactly one of a single time unit (year, month, ...).
+   * If the optional parameter `allowedTimeUnits` is given then only the units in there are allowed; all other return `false` as well.
+   * @param range The range (in ISO-8601 time span format) to be evaluated
+   * @param allowedTimeUnits Optional array of time units that are allowed for the single time unit.
+   *
+   * @example
+   * `P1Y1M` is a duration of one year AND one month which is more than one time unit; therefore is the result `false`
+   * `P2Y` is a duration of two years which is more than one of a single time unit; therefore is the result `false`
+   * `P1D` is a duration of one day which is exactly one of a single time unit; therefore the result is `true`
+   */
+  private isRangeExactlyOneOfSingleTimeUnit(range: string | null, allowedTimeUnits?: ManipulateType[]): boolean {
+    if (range) {
+      const rangeDuration = dayjs.duration(range);
+      const unit = TimeExtentUtil.extractUnitFromDuration(rangeDuration);
+      if (unit) {
+        const isAllowedUnit = !allowedTimeUnits || allowedTimeUnits.includes(unit);
+        const isOneTimeUnit = TimeExtentUtil.getDurationAsNumber(rangeDuration, unit) === 1;
+        return isAllowedUnit && isOneTimeUnit;
+      }
+    }
+    return false;
+  }
+
   private findPositionOfDate(date: Date): number | undefined {
     const index = this.availableDates.findIndex((availableDate) => Math.abs(dayjs(availableDate).diff(date)) === 0);
     return index === -1 ? undefined : index;
@@ -100,7 +133,7 @@ export class TimeSliderComponent implements OnInit {
   }
 
   private convertTimeExtentToString(timeExtent: TimeExtent): string {
-    return this.timeSliderConfiguration.range
+    return this.hasSimpleCurrentValue
       ? this.convertDateToString(timeExtent.start)
       : `${this.convertDateToString(timeExtent.start)} - ${this.convertDateToString(timeExtent.end)}`;
   }
