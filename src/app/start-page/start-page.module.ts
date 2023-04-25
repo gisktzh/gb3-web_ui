e6cb566041cff07a65780d8a90580e8dbacd034f
Sebastian Wendland <Sebastian.Wendland@ebp.ch>
2023-04-25 17:41:02 +0200

    GB3-559: Replaced datenkatalog-placeholder.png with a new version; Added start-page-placeholder.component which contains the temporary placeholder for the start page.

--- src/app/start-page/start-page.module.ts
+++ src/app/start-page/start-page.module.ts
@@ -7,9 +7,17 @@ import {TwitterFeedComponent} from './components/twitter-feed/twitter-feed.compo
 import {NewsFeedComponent} from './components/news-feed/news-feed.component';
 import {DiscoverMapsComponent} from './components/discover-maps/discover-maps.component';
 import {ContentLoadingStateComponent} from './components/content-loading-state/content-loading-state.component';
+import {StartPagePlaceholderComponent} from './components/start-page-placeholder/start-page-placeholder.component';
 
 @NgModule({
-  declarations: [StartPageComponent, TwitterFeedComponent, NewsFeedComponent, DiscoverMapsComponent, ContentLoadingStateComponent],
+  declarations: [
+    StartPageComponent,
+    TwitterFeedComponent,
+    NewsFeedComponent,
+    DiscoverMapsComponent,
+    ContentLoadingStateComponent,
+    StartPagePlaceholderComponent
+  ],
   imports: [CommonModule, SharedModule, StartPageRoutingModule]
 })
 export class StartPageModule {}
