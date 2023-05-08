5b79b6a54f7524d52bf4af19b68d4fc07d3ee6ee
Lukas Merz <lukemerz@gmail.com>
2023-05-08 14:38:13 +0200

    Add configuration for dev-ebp

--- src/app/shared/configs/runtime.dev-ebp.config.ts
+++ src/app/shared/configs/runtime.dev-ebp.config.ts
@@ -40,5 +40,42 @@ export const defaultRuntimeConfig: RuntimeConfig[] = [
       issuer: 'https://maps.zh.ch/'
     },
     overrides: {}
+  },
+  {
+    hostMatch: 'dev.geo.zh.ch',
+    apiBasePaths: {
+      gb2Api: {
+        baseUrl: 'https://maps.zh.ch/v3'
+      },
+      gb2StaticFiles: {
+        baseUrl: 'https://maps.zh.ch'
+      },
+      gb2Wms: {
+        baseUrl: 'https://wms.zh.ch'
+      },
+      geoLion: {
+        baseUrl: 'https://geolion.zh.ch'
+      },
+      searchApi: {
+        baseUrl: 'https://gb3-search-api.icycliff-4b8f6c95.switzerlandnorth.azurecontainerapps.io'
+      },
+      ktzhWebsite: {
+        baseUrl: 'https://www.zh.ch',
+        enabled: true
+      },
+      gravCms: {
+        baseUrl: 'https://gb3-grav-cms.icycliff-4b8f6c95.switzerlandnorth.azurecontainerapps.io',
+        enabled: false
+      },
+      twitterWidget: {
+        baseUrl: 'https://platform.twitter.com/widgets.js',
+        enabled: false
+      }
+    },
+    authSettings: {
+      clientId: 'gb3',
+      issuer: 'https://maps.zh.ch/'
+    },
+    overrides: {}
   }
 ];
