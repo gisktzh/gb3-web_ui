0b24ef42593f0c1f0d71f4f6ac70c1d68908db42
Lukas Merz <lukemerz@gmail.com>
2023-05-15 15:41:37 +0200

    Add more errorhandling; add disabled state

--- src/app/map/services/geolocation.service.ts
+++ src/app/map/services/geolocation.service.ts
@@ -11,29 +11,38 @@ const GEOLOCATION_TIMEOUT_IN_MS = 5000;
   providedIn: 'root'
 })
 export class GeolocationService {
-  private readonly navigator: Navigator;
+  private readonly navigator?: Navigator;
 
   constructor(
     @Inject(DOCUMENT) private readonly document: Document,
     @Inject(MAP_SERVICE) private readonly mapService: MapService,
     private readonly store: Store
   ) {
-    this.navigator = this.document.defaultView!.navigator;
+    if (this.document.defaultView) {
+      this.navigator = this.document.defaultView.navigator;
+    } else {
+      console.error('Navigator not available.'); // todo: error handling
+    }
   }
 
   public getPosition() {
-    this.navigator.geolocation.getCurrentPosition(
-      (position) => {
-        // Geolocation API always returns 4326 coordinates
-        const location: PointWithSrs = {coordinates: [position.coords.longitude, position.coords.latitude], srs: 4326, type: 'Point'};
-        this.store.dispatch(GeolocationActions.setSuccess({location}));
-      },
-      (error) => {
-        this.store.dispatch(GeolocationActions.setFailure({error}));
-      },
-      {
-        timeout: GEOLOCATION_TIMEOUT_IN_MS
-      }
-    );
+    if (!this.navigator) {
+      // todo: at some point in the future, we need to check whether this works - so we need a browser that does not have the navigator API.
+      this.store.dispatch(GeolocationActions.setFailure({error: new GeolocationPositionError()}));
+    } else {
+      this.navigator.geolocation.getCurrentPosition(
+        (position) => {
+          // Geolocation API always returns 4326 coordinates
+          const location: PointWithSrs = {coordinates: [position.coords.longitude, position.coords.latitude], srs: 4326, type: 'Point'};
+          this.store.dispatch(GeolocationActions.setSuccess({location}));
+        },
+        (error) => {
+          this.store.dispatch(GeolocationActions.setFailure({error}));
+        },
+        {
+          timeout: GEOLOCATION_TIMEOUT_IN_MS
+        }
+      );
+    }
   }
 }
