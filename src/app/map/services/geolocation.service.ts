6b3adfaf3b92c6f5e1c69b26f78f2067f4d66c6b
Lukas Merz <lukemerz@gmail.com>
2023-05-15 15:35:31 +0200

    Add timeout for geolocation request

--- src/app/map/services/geolocation.service.ts
+++ src/app/map/services/geolocation.service.ts
@@ -6,6 +6,7 @@ import {MapService} from '../interfaces/map.service';
 import {PointWithSrs} from '../../shared/interfaces/geojson-types-with-srs.interface';
 import {GeolocationActions} from '../../state/map/actions/geolocation.actions';
 
+const GEOLOCATION_TIMEOUT_IN_MS = 5000;
 @Injectable({
   providedIn: 'root'
 })
@@ -23,11 +24,15 @@ export class GeolocationService {
   public getPosition() {
     this.navigator.geolocation.getCurrentPosition(
       (position) => {
+        // Geolocation API always returns 4326 coordinates
         const location: PointWithSrs = {coordinates: [position.coords.longitude, position.coords.latitude], srs: 4326, type: 'Point'};
         this.store.dispatch(GeolocationActions.setSuccess({location}));
       },
       (error) => {
         this.store.dispatch(GeolocationActions.setFailure({error}));
+      },
+      {
+        timeout: GEOLOCATION_TIMEOUT_IN_MS
       }
     );
   }
