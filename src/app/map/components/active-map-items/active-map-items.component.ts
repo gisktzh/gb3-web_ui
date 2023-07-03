487cc7de90d06da9209670d27394ef634a90472a
Sebastian Wendland <Sebastian.Wendland@ebp.ch>
2023-07-03 13:42:12 +0200

    Refactored map-ui.actions by adding all `MatDialog` inside the map UI.

--- src/app/map/components/active-map-items/active-map-items.component.ts
+++ src/app/map/components/active-map-items/active-map-items.component.ts
@@ -6,19 +6,14 @@ import {selectActiveMapItems} from '../../../state/map/reducers/active-map-item.
 import {Subscription, tap} from 'rxjs';
 import {ActiveMapItem} from '../../models/active-map-item.model';
 import {selectIsAuthenticated} from '../../../state/auth/reducers/auth-status.reducer';
-import {MatDialog} from '@angular/material/dialog';
-import {FavouriteCreationDialogComponent} from '../favourite-creation-dialog/favourite-creation-dialog.component';
-import {PanelClass} from '../../../shared/enums/panel-class.enum';
-import {MapNoticeDialogComponent} from '../map-notice-dialog/map-notice-dialog.component';
 import {isActiveMapItemOfType} from '../../../shared/type-guards/active-map-item-type.type-guard';
 import {Gb2WmsActiveMapItem} from '../../models/implementations/gb2-wms.model';
+import {MapUiActions} from '../../../state/map/actions/map-ui.actions';
 
 const FAVOURITE_HELPER_MESSAGES = {
   noMapsAdded: 'Fügen Sie mindestens 1 Karte hinzu, um einen Favoriten anzulegen.',
   notAuthenticated: 'Loggen Sie sich ein, um Favoriten hinzuzufügen.'
 };
-const FAVOURITE_CREATION_DIALOG_MAX_WIDTH = 500;
-const MAP_NOTICES_DIALOG_MAX_WIDTH = 968;
 
 @Component({
   selector: 'active-map-items',
@@ -37,7 +32,7 @@ export class ActiveMapItemsComponent implements OnInit, OnDestroy {
   private readonly isAuthenticated$ = this.store.select(selectIsAuthenticated);
   private readonly subscriptions: Subscription = new Subscription();
 
-  constructor(private readonly store: Store, private readonly dialogService: MatDialog) {}
+  constructor(private readonly store: Store) {}
 
   public ngOnInit() {
     this.initSubscriptions();
@@ -62,11 +57,7 @@ export class ActiveMapItemsComponent implements OnInit, OnDestroy {
   }
 
   public showFavouriteDialog() {
-    this.dialogService.open(FavouriteCreationDialogComponent, {
-      panelClass: PanelClass.ApiWrapperDialog,
-      restoreFocus: false,
-      maxWidth: FAVOURITE_CREATION_DIALOG_MAX_WIDTH
-    });
+    this.store.dispatch(MapUiActions.showCreateFavouriteDialog());
   }
 
   public toggleMinimizeActiveMapItems() {
@@ -74,13 +65,7 @@ export class ActiveMapItemsComponent implements OnInit, OnDestroy {
   }
 
   public showMapNotices() {
-    this.store.dispatch(ActiveMapItemActions.markAllActiveMapItemNoticeAsRead());
-    this.dialogService.open(MapNoticeDialogComponent, {
-      panelClass: PanelClass.ApiWrapperDialog,
-      restoreFocus: false,
-      data: this.activeMapItems.filter((activeMapItem) => activeMapItem.settings.type === 'gb2Wms' && activeMapItem.settings.notice), // todo: As soon as more layers with notices come into play, a selector on the interface would be required.
-      maxWidth: MAP_NOTICES_DIALOG_MAX_WIDTH
-    });
+    this.store.dispatch(MapUiActions.showMapNoticesDialog());
   }
 
   private initSubscriptions() {
--- src/app/state/map/effects/map-ui.effects.ts
+++ src/app/state/map/effects/map-ui.effects.ts
@@ -10,6 +10,16 @@ import {tap} from 'rxjs';
 import {ShareLinkDialogComponent} from '../../../map/components/share-link-dialog/share-link-dialog.component';
 import {PanelClass} from '../../../shared/enums/panel-class.enum';
 import {MatDialog} from '@angular/material/dialog';
+import {FavouriteCreationDialogComponent} from '../../../map/components/favourite-creation-dialog/favourite-creation-dialog.component';
+import {MapNoticeDialogComponent} from '../../../map/components/map-notice-dialog/map-notice-dialog.component';
+import {ActiveMapItemActions} from '../actions/active-map-item.actions';
+import {selectGb2WmsActiveMapItemsWithMapNotices} from '../selectors/active-map-items.selector';
+import {FavouriteDeletionDialogComponent} from '../../../map/components/favourite-deletion-dialog/favourite-deletion-dialog.component';
+import {Favourite} from '../../../shared/interfaces/favourite.interface';
+
+const CREATE_FAVOURITE_DIALOG_MAX_WIDTH = 500;
+const DELETE_FAVOURITE_DIALOG_MAX_WIDTH = 500;
+const MAP_NOTICES_DIALOG_MAX_WIDTH = 968;
 
 @Injectable()
 export class MapUiEffects {
@@ -55,6 +65,57 @@ export class MapUiEffects {
       concatLatestFrom(() => this.store.select(selectCurrentShareLinkItem)),
       map(([_, shareLinkItem]) => {
         return ShareLinkActions.createShareLinkId({shareLinkItem});
+      })
+    );
+  });
+
+  public dispatchShowCreateFavouriteDialogRequest$ = createEffect(
+    () => {
+      return this.actions$.pipe(
+        ofType(MapUiActions.showCreateFavouriteDialog),
+        tap(() =>
+          this.dialogService.open(FavouriteCreationDialogComponent, {
+            panelClass: PanelClass.ApiWrapperDialog,
+            restoreFocus: false,
+            maxWidth: CREATE_FAVOURITE_DIALOG_MAX_WIDTH
+          })
+        )
+      );
+    },
+    {dispatch: false}
+  );
+
+  public dispatchShowDeleteFavouriteDialogRequest$ = createEffect(
+    () => {
+      return this.actions$.pipe(
+        ofType(MapUiActions.showDeleteFavouriteDialog),
+        tap(({favouriteToDelete}) =>
+          this.dialogService.open<FavouriteDeletionDialogComponent, {favourite: Favourite}, boolean>(FavouriteDeletionDialogComponent, {
+            data: {favourite: favouriteToDelete},
+            panelClass: PanelClass.ApiWrapperDialog,
+            restoreFocus: false,
+            maxWidth: DELETE_FAVOURITE_DIALOG_MAX_WIDTH
+          })
+        )
+      );
+    },
+    {dispatch: false}
+  );
+
+  public dispatchShowMapNoticesDialogRequest$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(MapUiActions.showMapNoticesDialog),
+      concatLatestFrom(() => this.store.select(selectGb2WmsActiveMapItemsWithMapNotices)),
+      tap(([_, gb2WmsActiveMapItemsWithMapNotices]) =>
+        this.dialogService.open(MapNoticeDialogComponent, {
+          panelClass: PanelClass.ApiWrapperDialog,
+          restoreFocus: false,
+          data: gb2WmsActiveMapItemsWithMapNotices,
+          maxWidth: MAP_NOTICES_DIALOG_MAX_WIDTH
+        })
+      ),
+      map(() => {
+        return ActiveMapItemActions.markAllActiveMapItemNoticeAsRead();
       })
     );
   });
