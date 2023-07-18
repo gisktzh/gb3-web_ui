17b0e63937c74892df0639b25e0fabdc0f6a57fb
Lukas Merz <lukemerz@gmail.com>
2023-07-18 09:35:19 +0200

    GB3-673: Updated basemaps

--- src/app/shared/configs/base-map.config.ts
+++ src/app/shared/configs/base-map.config.ts
@@ -9,177 +9,212 @@ export const defaultBasemaps: Basemap[] = [
   {
     id: `${MapConstants.INTERNAL_LAYER_PREFIX}blank`,
     type: 'blank',
-    title: 'Leer'
+    title: 'Kein Hintergrund',
+  },
+  {
+    id: 'areosmbackgroundzh',
+    type: 'wms',
+    relativeImagePath: 'assets/images/basemaps/areosmbackgroundzh.png',
+    url: 'https://maps.zh.ch/wms/AREOSMBackgroundZH',
+    title: 'Situationsplan',
+    srsId: 2056,
+    layers: [
+      {
+        name: 'osm_mundial_terrestris',
+      },
+      {
+        name: 'ch_osm_buildings',
+      },
+      {
+        name: 'ch_osm_highways',
+      },
+      {
+        name: 'ch_osm_landuse',
+      },
+      {
+        name: 'grenzen',
+      },
+      {
+        name: 'gemeindegrenzen',
+      },
+    ],
+  },
+  {
+    id: 'areavbackgroundzh',
+    type: 'wms',
+    relativeImagePath: 'assets/images/basemaps/areavbackgroundzh.png',
+    url: 'https://maps.zh.ch/wms/AREAVBackgroundZH',
+    title: 'Amtliche Vermessung',
+    srsId: 2056,
+    layers: [
+      {
+        name: 'wald',
+      },
+      {
+        name: 'seen',
+      },
+      {
+        name: 'grenzen',
+      },
+      {
+        name: 'gemeindegrenzen',
+      },
+      {
+        name: 'lk500',
+      },
+      {
+        name: 'lk200',
+      },
+      {
+        name: 'lk100',
+      },
+      {
+        name: 'lk50',
+      },
+      {
+        name: 'lk25',
+      },
+      {
+        name: 'LCSF',
+      },
+      {
+        name: 'SOSF',
+      },
+      {
+        name: 'SOLI',
+      },
+      {
+        name: 'LOCPOS',
+      },
+      {
+        name: 'HADR',
+      },
+      {
+        name: 'LNNA',
+      },
+      {
+        name: 'LNNA_Ortsname',
+      },
+      {
+        name: 'OSNR_liegenschaften',
+      },
+      {
+        name: 'RESF',
+      },
+      {
+        name: 'MBSF',
+      },
+      {
+        name: 'TBLI',
+      },
+      {
+        name: 'OSBP',
+      },
+      {
+        name: 'TBBP',
+      },
+    ],
   },
   {
     id: 'aredtmbackgroundzh',
     type: 'wms',
     relativeImagePath: 'assets/images/basemaps/aredtmbackgroundzh.png',
     url: 'https://maps.zh.ch/wms/AREDTMBackgroundZH',
-    title: 'Digitales Terrainmodell',
+    title: 'Gelände',
     srsId: 2056,
     layers: [
       {
-        name: 'dtm2022_relief'
-      }
-    ]
+        name: 'dtm_swisstopo',
+      },
+      {
+        name: 'dtm_zh_aktuell',
+      },
+      {
+        name: 'grenzen',
+      },
+      {
+        name: 'gemeindegrenzen',
+      },
+    ],
+  },
+  {
+    id: 'areorthobackgroundzh',
+    type: 'wms',
+    relativeImagePath: 'assets/images/basemaps/areorthobackgroundzh.png',
+    url: 'https://maps.zh.ch/wms/AREOrthoBackgroundZH',
+    title: 'Luftbild',
+    srsId: 2056,
+    layers: [
+      {
+        name: 'ortho_swisstopo',
+      },
+      {
+        name: 'ortho_zh_aktuell',
+      },
+      {
+        name: 'grenzen',
+      },
+      {
+        name: 'gemeindegrenzen',
+      },
+    ],
   },
   {
     id: 'arewildbackgroundzh',
     type: 'wms',
     relativeImagePath: 'assets/images/basemaps/arewildbackgroundzh.png',
     url: 'https://maps.zh.ch/wms/AREWildBackgroundZH',
-    title: 'Historische Karte J. Wild',
+    title: 'Historische Karte J. Wild (~1850)',
     srsId: 2056,
     layers: [
       {
-        name: 'wild'
-      }
-    ]
-  },
-  {
-    id: 'areupbackgroundzh',
-    type: 'wms',
-    relativeImagePath: 'assets/images/basemaps/areupbackgroundzh.png',
-    url: 'https://maps.zh.ch/wms/AREUPBackgroundZH',
-    title: 'Übersichtsplan',
-    srsId: 2056,
-    layers: [
-      {
-        name: 'lk500'
+        name: 'wild',
       },
-      {
-        name: 'lk200'
-      },
-      {
-        name: 'lk100'
-      },
-      {
-        name: 'lk50'
-      },
-      {
-        name: 'lk25'
-      },
-      {
-        name: 'grenzen'
-      },
-      {
-        name: 'gemeindegrenzen'
-      },
-      {
-        name: 'upnk_eisenbahn'
-      },
-      {
-        name: 'upnk_hsfl'
-      },
-      {
-        name: 'upzh_strassen'
-      },
-      {
-        name: 'upzh_eisenbahn'
-      },
-      {
-        name: 'upzh_einzelobjekte_l'
-      },
-      {
-        name: 'upzh_befestigte_flaeche'
-      },
-      {
-        name: 'upzh_unbefestigte_flaeche'
-      },
-      {
-        name: 'upzh_gewaesser_l'
-      },
-      {
-        name: 'upzh_vegetationslos'
-      },
-      {
-        name: 'upzh_wald_p'
-      },
-      {
-        name: 'upzh_waldgrenze'
-      },
-      {
-        name: 'upnk_gebaeude'
-      },
-      {
-        name: 'upnk_strassen'
-      },
-      {
-        name: 'upnk_wald'
-      },
-      {
-        name: 'upnk_gewaesser'
-      },
-      {
-        name: 'upnk_befestigte_flaeche'
-      },
-      {
-        name: 'upzh_fels_boeschung'
-      },
-      {
-        name: 'upzh_gewaesser_f'
-      },
-      {
-        name: 'upzh_gebaeude'
-      },
-      {
-        name: 'upzh_weitere_gebaeude'
-      },
-      {
-        name: 'upzh_weitereFlaechen'
-      },
-      {
-        name: 'upzh_liegenschaften'
-      },
-      {
-        name: 'Hoehenlinien'
-      },
-      {
-        name: 'upzh_beschriftung'
-      },
-      {
-        name: 'upnk_beschriftung'
-      }
-    ]
+    ],
   },
   {
     id: 'arelkbackgroundzh',
     type: 'wms',
     relativeImagePath: 'assets/images/basemaps/arelkbackgroundzh.png',
     url: 'https://maps.zh.ch/wms/ARElkBackgroundZH',
-    title: 'Landeskarte',
+    title: 'Basiskarte',
     srsId: 2056,
     layers: [
       {
-        name: 'lk500'
+        name: 'wald',
       },
       {
-        name: 'lk200'
+        name: 'seen',
       },
       {
-        name: 'lk100'
+        name: 'grenzen',
       },
       {
-        name: 'lk50'
+        name: 'gemeindegrenzen',
       },
       {
-        name: 'lk25'
+        name: 'lk500',
       },
       {
-        name: 'up8'
+        name: 'lk200',
       },
       {
-        name: 'up24'
+        name: 'lk100',
       },
       {
-        name: 'grenzen'
+        name: 'lk50',
       },
       {
-        name: 'gemeindegrenzen'
-      }
-    ]
-  }
+        name: 'lk25',
+      },
+      {
+        name: 'up8',
+      },
+      {
+        name: 'up24',
+      },
+    ],
+  },
 ];
 
-export const defaultBasemap = defaultBasemaps[4];
+export const defaultBasemap = defaultBasemaps[6];
