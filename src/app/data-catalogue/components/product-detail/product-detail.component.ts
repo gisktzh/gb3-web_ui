69182fc180a76370e02266d366c52877f58984ab
Lukas Merz <lukemerz@gmail.com>
2023-07-19 09:50:51 +0200

    GB3-695: Add product detail

--- src/app/data-catalogue/components/product-detail/product-detail.component.ts
+++ src/app/data-catalogue/components/product-detail/product-detail.component.ts
@@ -1,10 +1,23 @@
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {ProductMetadata} from '../../../shared/interfaces/gb3-metadata.interface';
 import {LoadingState} from '../../../shared/types/loading-state';
 import {Subscription, switchMap, tap, throwError} from 'rxjs';
 import {ActivatedRoute} from '@angular/router';
 import {Gb3MetadataService} from '../../../shared/services/apis/gb3/gb3-metadata.service';
 import {ConfigService} from '../../../shared/services/config.service';
+import {DepartmentalContact, ProductMetadata} from '../../../shared/interfaces/gb3-metadata.interface';
+import {MainPage} from '../../../shared/enums/main-page.enum';
+import {DataCataloguePage} from '../../../shared/enums/data-catalogue-page.enum';
+import {DataDisplayElement} from '../data-display/data-display.component';
+
+interface DatasetInformation {
+  title: string;
+}
+
+interface DataLink {
+  name: string;
+  guid: number;
+  shortDescription: string;
+}
 
 @Component({
   selector: 'product-detail',
@@ -12,15 +25,20 @@ import {ConfigService} from '../../../shared/services/config.service';
   styleUrls: ['./product-detail.component.scss'],
 })
 export class ProductDetailComponent implements OnInit, OnDestroy {
-  public data: ProductMetadata | undefined;
+  public datasetInformation: DatasetInformation | undefined;
+  public metadataContactElements: DataDisplayElement[] = [];
+  public informationElements: DataDisplayElement[] = [];
+  public linkedDatasets: DataLink[] = [];
   public loadingState: LoadingState = 'loading';
   public readonly apiBaseUrl: string;
+  public readonly mainPageEnum = MainPage;
+  public readonly dataCataloguePageEnum = DataCataloguePage;
   private readonly subscriptions: Subscription = new Subscription();
 
   constructor(
     private readonly route: ActivatedRoute,
     private readonly gb3MetadataService: Gb3MetadataService,
-    private readonly configService: ConfigService
+    private readonly configService: ConfigService,
   ) {
     this.apiBaseUrl = this.configService.apiConfig.gb2StaticFiles.baseUrl;
   }
@@ -45,11 +63,45 @@ export class ProductDetailComponent implements OnInit, OnDestroy {
             return this.gb3MetadataService.loadProductDetail(id);
           }),
           tap((results) => {
-            this.data = results;
+            this.datasetInformation = this.extractDatasetInformation(results);
+            this.metadataContactElements = this.extractContactElements(results.contact.metadata);
+            this.informationElements = this.extractInformationElements(results);
+            this.linkedDatasets = this.extractLinkedDatasets(results);
             this.loadingState = 'loaded';
-          })
+          }),
         )
-        .subscribe()
+        .subscribe(),
     );
+  }
+
+  private extractContactElements(contact: DepartmentalContact): DataDisplayElement[] {
+    return [
+      {title: 'Organisation', value: contact.department, type: 'text'},
+      {title: 'Abteilung', value: contact.division, type: 'text'},
+      {title: 'Kontaktperson', value: `${contact.firstName} ${contact.lastName}`, type: 'text'},
+      {title: 'Adresse', value: `${contact.street} ${contact.houseNumber}, ${contact.zipCode} ${contact.village}`, type: 'text'},
+      {title: 'Tel', value: contact.phone, type: 'text'},
+      {title: 'Tel direkt', value: contact.phoneDirect, type: 'text'},
+      {title: 'E-Mail', value: contact.email, type: 'email'},
+      {title: 'www', value: contact.url, type: 'url'},
+    ];
+  }
+
+  private extractLinkedDatasets({datasets}: ProductMetadata): DataLink[] {
+    return datasets.map((dataset) => ({...dataset}));
+  }
+
+  private extractInformationElements(data: ProductMetadata): DataDisplayElement[] {
+    return [
+      {title: 'GIS-ZH Nr.', value: data.guid.toString(), type: 'text'},
+      {title: 'Bezeichnung', value: data.name, type: 'text'},
+      {title: 'Beschreibung', value: data.description, type: 'text'},
+    ];
+  }
+
+  private extractDatasetInformation(results: ProductMetadata): DatasetInformation {
+    return {
+      title: results.name,
+    };
   }
 }
