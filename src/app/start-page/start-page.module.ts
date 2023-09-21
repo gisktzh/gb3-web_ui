91825453e9b975d75f84489e6703370cc424d4ea
Sebastian Wendland <Sebastian.Wendland@ebp.ch>
2023-09-21 15:18:51 +0200

    GB3-330: Removed unnecessary placeholder.

--- src/app/start-page/start-page.module.ts
+++ src/app/start-page/start-page.module.ts
@@ -7,7 +7,6 @@ import {TwitterFeedComponent} from './components/twitter-feed/twitter-feed.compo
 import {NewsFeedComponent} from './components/news-feed/news-feed.component';
 import {DiscoverMapsComponent} from './components/discover-maps/discover-maps.component';
 import {ContentLoadingStateComponent} from './components/content-loading-state/content-loading-state.component';
-import {StartPagePlaceholderComponent} from './components/start-page-placeholder/start-page-placeholder.component';
 import {GisBrowserTeaserComponent} from './components/gis-browser-teaser/gis-browser-teaser.component';
 import {FrequentlyUsedItemsComponent} from './components/frequently-used-items/frequently-used-items.component';
 import {StartPageSearchOverlayComponent} from './components/start-page-search-overlay/start-page-search-overlay.component';
@@ -20,7 +19,6 @@ import {SearchResultGroupComponent} from './components/search-result-group/searc
     NewsFeedComponent,
     DiscoverMapsComponent,
     ContentLoadingStateComponent,
-    StartPagePlaceholderComponent,
     GisBrowserTeaserComponent,
     FrequentlyUsedItemsComponent,
     StartPageSearchOverlayComponent,
