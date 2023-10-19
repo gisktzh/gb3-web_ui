e3708ad9dc38ae9c8d5b40ac0f722887d6a43707
till_schuetze <till.schuetze@ebp.ch>
2023-10-19 17:03:17 +0200

    GB3-377: add some styling to the components and allow info and delete button on active map item to be clicked immediately

--- src/app/map/components/active-map-items/active-map-item-header/active-map-item-header.component.ts
+++ src/app/map/components/active-map-items/active-map-item-header/active-map-item-header.component.ts
@@ -1,24 +1,38 @@
-import {Component, Input} from '@angular/core';
-import {ActiveMapItem} from '../../../models/active-map-item.model';
+import {Component, Input, OnDestroy, OnInit} from '@angular/core';
 import {MatExpansionPanel} from '@angular/material/expansion';
-import {ActiveMapItemActions} from '../../../../state/map/actions/active-map-item.actions';
 import {Store} from '@ngrx/store';
-import {MainPage} from '../../../../shared/enums/main-page.enum';
+import {Subscription, tap} from 'rxjs';
+import {ScreenMode} from 'src/app/shared/types/screen-size.type';
+import {selectScreenMode} from 'src/app/state/app/reducers/app-layout.reducer';
 import {DataCataloguePage} from '../../../../shared/enums/data-catalogue-page.enum';
+import {MainPage} from '../../../../shared/enums/main-page.enum';
+import {ActiveMapItemActions} from '../../../../state/map/actions/active-map-item.actions';
+import {ActiveMapItem} from '../../../models/active-map-item.model';
 
 @Component({
   selector: 'active-map-item-header',
   templateUrl: './active-map-item-header.component.html',
   styleUrls: ['./active-map-item-header.component.scss'],
 })
-export class ActiveMapItemHeaderComponent {
+export class ActiveMapItemHeaderComponent implements OnInit, OnDestroy {
   @Input() public activeMapItem!: ActiveMapItem;
   @Input() public activeMapItemExpansionPanel!: MatExpansionPanel;
+  public screenMode: ScreenMode = 'regular';
+
+  private readonly screenMode$ = this.store.select(selectScreenMode);
+  private readonly subscriptions: Subscription = new Subscription();
 
   protected readonly mainPageEnum = MainPage;
   protected readonly dataCataloguePageEnum = DataCataloguePage;
 
   constructor(private readonly store: Store) {}
+
+  public ngOnInit(): void {
+    this.subscriptions.add(this.screenMode$.pipe(tap((screenMode) => (this.screenMode = screenMode))).subscribe());
+  }
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 
   public toggleMapItemVisibility(activeMapItem: ActiveMapItem) {
     this.store.dispatch(ActiveMapItemActions.setVisibility({visible: !activeMapItem.visible, activeMapItem}));
