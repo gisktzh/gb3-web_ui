0fddf7d5c80b7d8638895023be26d449c051ad6d
till_schuetze <till.schuetze@ebp.ch>
2023-10-24 10:06:29 +0200

    GB3-858: Update some css and remove unused variables from navbar

--- src/app/shared/components/navbar-mobile/navbar-mobile.component.ts
+++ src/app/shared/components/navbar-mobile/navbar-mobile.component.ts
@@ -1,6 +1,5 @@
 import {Component} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
-import {MainPage} from '../../enums/main-page.enum';
 import {NavbarMobileDialogComponent} from './navbar-mobile-dialog/navbar-mobile-dialog.component';
 
 @Component({
@@ -9,12 +8,10 @@ import {NavbarMobileDialogComponent} from './navbar-mobile-dialog/navbar-mobile-
   styleUrls: ['./navbar-mobile.component.scss'],
 })
 export class NavbarMobileComponent {
-  protected readonly mainPageEnum = MainPage;
-
   constructor(public dialog: MatDialog) {}
 
   showMenu() {
-    const dialogRef = this.dialog.open(NavbarMobileDialogComponent, {
+    this.dialog.open(NavbarMobileDialogComponent, {
       maxWidth: '100vw',
       maxHeight: '100vh',
       height: '100%',
