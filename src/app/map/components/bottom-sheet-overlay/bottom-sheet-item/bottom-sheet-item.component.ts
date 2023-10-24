f28a52cfb85e1f994583cfbd1f5b5e0f92931933
till_schuetze <till.schuetze@ebp.ch>
2023-10-24 09:25:40 +0200

    GB3-858: rename isSearching to is Filtering, rename css class
    (cherry picked from commit 2c8abf0407f7bdf21a810242b5219335c05d2526)

--- src/app/map/components/bottom-sheet-overlay/bottom-sheet-item/bottom-sheet-item.component.ts
+++ src/app/map/components/bottom-sheet-overlay/bottom-sheet-item/bottom-sheet-item.component.ts
@@ -5,7 +5,7 @@ import {BottomSheetHeight} from 'src/app/shared/types/bottom-sheet-height.type';
 import {ResizeHandlerLocation} from 'src/app/shared/types/resize-handler-location.type';
 import {StyleExpression} from 'src/app/shared/types/style-expression.type';
 import {MapUiActions} from 'src/app/state/map/actions/map-ui.actions';
-import {selectIsSearching} from 'src/app/state/map/reducers/layer-catalog.reducer';
+import {selectIsFiltering} from 'src/app/state/map/reducers/layer-catalog.reducer';
 import {selectTitle} from 'src/app/state/map/reducers/map-attribute-filters-item.reducer';
 
 @Component({
@@ -17,11 +17,11 @@ export class BottomSheetItemComponent implements OnInit, OnDestroy {
   @Input() public overlayTitle?: string = '';
   @Input() public usePrimaryColor: boolean = false;
   @Input() public bottomSheetHeight: BottomSheetHeight = 'small';
-  @Input() showHeader: boolean = true;
-  public isSearching: boolean = false;
+  @Input() public showHeader: boolean = true;
+  public isFiltering: boolean = false;
   public mapAttributeFiltersItemTitle: string = '';
 
-  private readonly isSearching$ = this.store.select(selectIsSearching);
+  private readonly isFiltering$ = this.store.select(selectIsFiltering);
   private readonly mapAttributeFiltersItemTitle$ = this.store.select(selectTitle);
   private readonly subscriptions: Subscription = new Subscription();
 
@@ -53,6 +53,6 @@ export class BottomSheetItemComponent implements OnInit, OnDestroy {
         .pipe(tap((mapAttributeFiltersItemTitle) => (this.mapAttributeFiltersItemTitle = mapAttributeFiltersItemTitle)))
         .subscribe(),
     );
-    this.subscriptions.add(this.isSearching$.pipe(tap((isSearching) => (this.isSearching = isSearching))).subscribe());
+    this.subscriptions.add(this.isFiltering$.pipe(tap((isFiltering) => (this.isFiltering = isFiltering))).subscribe());
   }
 }
