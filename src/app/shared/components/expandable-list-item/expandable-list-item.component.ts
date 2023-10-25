92646e6567110ce00f07d390717934521cdc3722
till_schuetze <till.schuetze@ebp.ch>
2023-10-25 10:49:59 +0200

    GB3-858: Use expandable-list-item in saerch results

--- src/app/shared/components/expandable-list-item/expandable-list-item.component.ts
+++ src/app/shared/components/expandable-list-item/expandable-list-item.component.ts
@@ -1,5 +1,6 @@
 import {Component, Input} from '@angular/core';
-import {Topic} from '../../interfaces/topic.interface';
+import {Map, Topic} from '../../interfaces/topic.interface';
+import {GeometrySearchApiResultMatch} from '../../services/apis/search/interfaces/search-api-result-match.interface';
 import {LoadingState} from '../../types/loading-state.type';
 
 const AUTO_OPEN_THRESHOLD = 3;
@@ -13,7 +14,12 @@ export class ExpandableListItemComponent {
   @Input() public topic: Topic = {title: '', maps: []};
   @Input() public expanded: boolean = false;
   @Input() public header: string = '';
-  @Input() filterString: string = '';
+  @Input() public badge: number = 0;
+  @Input() public filterString: string = '';
+  @Input() public filteredMaps: Map[] = [];
+  @Input() public searchResults: GeometrySearchApiResultMatch[] = [];
+  @Input() public type: string = '';
+
   @Input() public loadingState: LoadingState;
 
   public autoOpenThreshold: number = AUTO_OPEN_THRESHOLD;
