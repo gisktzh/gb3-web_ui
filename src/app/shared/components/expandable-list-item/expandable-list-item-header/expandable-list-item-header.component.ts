e1749223492a319a6c9b86ac5a8440537ff88066
till_schuetze <till.schuetze@ebp.ch>
2023-10-31 09:17:54 +0100

    GB3-858: make inputs for expandable list item more generic

--- src/app/shared/components/expandable-list-item/expandable-list-item-header/expandable-list-item-header.component.ts
+++ src/app/shared/components/expandable-list-item/expandable-list-item-header/expandable-list-item-header.component.ts
@@ -9,8 +9,8 @@ import {LoadingState} from 'src/app/shared/types/loading-state.type';
 export class ExpandableListItemHeaderComponent {
   @Input() public title!: string;
   @Input() public filterString: string = '';
+  @Input() public isFilter: boolean = false;
   @Input() public isExpanded: boolean = true;
   @Input() public loadingState: LoadingState;
   @Input() public numberOfItems: number = 0;
-  @Input() public hasResults: boolean = false;
 }
