8ab1a6646688c05a2d5f339489db1d75625b8fa4
till_schuetze <till.schuetze@ebp.ch>
2023-10-31 10:56:57 +0100

    GB3-858: reset the filter String on screenMode change

--- src/app/state/app/effects/app-layout.effects.ts
+++ src/app/state/app/effects/app-layout.effects.ts
@@ -1,6 +1,7 @@
 import {Actions, createEffect, ofType} from '@ngrx/effects';
 import {Injectable} from '@angular/core';
 import {AppLayoutActions} from '../actions/app-layout.actions';
+import {LayerCatalogActions} from '../../map/actions/layer-catalog.actions';
 import {map} from 'rxjs';
 import {MapUiActions} from '../../map/actions/map-ui.actions';
 
@@ -11,6 +12,14 @@ export class AppLayoutEffects {
       ofType(AppLayoutActions.setScreenMode),
       map(() => {
         return MapUiActions.resetMapUiState();
+      }),
+    );
+  });
+  public resetFilterStringOnScreenModeChange$ = createEffect(() => {
+    return this.actions$.pipe(
+      ofType(AppLayoutActions.setScreenMode),
+      map(() => {
+        return LayerCatalogActions.setFilterString({filterString: undefined});
       }),
     );
   });
