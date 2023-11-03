e9f939985ea95cc3add16bc190e4ec564690e4d7
till_schuetze <till.schuetze@ebp.ch>
2023-11-03 07:57:58 +0100

    GB3-812: Add the rotation button to mobile. Design and location only temporary as some changes will be necessary due to mobile design finetuning

--- src/app/map/components/map-controls/map-rotation-button/map-rotation-button.component.ts
+++ src/app/map/components/map-controls/map-rotation-button/map-rotation-button.component.ts
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
 import {Store} from '@ngrx/store';
 import {Subscription, tap} from 'rxjs';
+import {ScreenMode} from 'src/app/shared/types/screen-size.type';
+import {selectScreenMode} from 'src/app/state/app/reducers/app-layout.reducer';
 import {MapConfigActions} from 'src/app/state/map/actions/map-config.actions';
 import {selectRotation} from '../../../../state/map/reducers/map-config.reducer';
 
@@ -11,9 +13,11 @@ import {selectRotation} from '../../../../state/map/reducers/map-config.reducer'
 })
 export class MapRotationButtonComponent {
   public rotation: number = 0;
+  public screenMode: ScreenMode = 'regular';
 
   private readonly subscriptions: Subscription = new Subscription();
   private readonly rotation$ = this.store.select(selectRotation);
+  private readonly screenMode$ = this.store.select(selectScreenMode);
 
   constructor(private readonly store: Store) {}
 
@@ -31,5 +35,6 @@ export class MapRotationButtonComponent {
 
   private initSubscriptions() {
     this.subscriptions.add(this.rotation$.pipe(tap((rotation) => (this.rotation = rotation))).subscribe());
+    this.subscriptions.add(this.screenMode$.pipe(tap((screenMode) => (this.screenMode = screenMode))).subscribe());
   }
 }
