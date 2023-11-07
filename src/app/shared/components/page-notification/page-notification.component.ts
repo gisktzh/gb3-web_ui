3db8c6102e342a81fb7750b074f6489c4c65838c
till_schuetze <till.schuetze@ebp.ch>
2023-11-07 08:48:26 +0100

    GB3-377: Page notification line limit changed back to 2 lines for desktop, 6 lines for mobile

--- src/app/shared/components/page-notification/page-notification.component.ts
+++ src/app/shared/components/page-notification/page-notification.component.ts
@@ -1,21 +1,37 @@
-import {Component, Inject} from '@angular/core';
+import {Component, Inject, OnDestroy, OnInit} from '@angular/core';
 import {MAT_SNACK_BAR_DATA} from '@angular/material/snack-bar';
 import {PageNotification, PageNotificationSeverity} from '../../interfaces/page-notification.interface';
 import {Store} from '@ngrx/store';
 import {PageNotificationActions} from '../../../state/app/actions/page-notification.actions';
+import {Subscription, tap} from 'rxjs';
+import {selectScreenMode} from 'src/app/state/app/reducers/app-layout.reducer';
+import {ScreenMode} from '../../types/screen-size.type';
 
 @Component({
   selector: 'page-notification',
   templateUrl: './page-notification.component.html',
   styleUrls: ['./page-notification.component.scss'],
 })
-export class PageNotificationComponent {
+export class PageNotificationComponent implements OnInit, OnDestroy {
   public readonly icon: string;
+  public screenMode: ScreenMode = 'regular';
+
+  private readonly subscriptions: Subscription = new Subscription();
+  private readonly screenMode$ = this.store.select(selectScreenMode);
+
   constructor(
     @Inject(MAT_SNACK_BAR_DATA) public data: PageNotification,
     private readonly store: Store,
   ) {
     this.icon = this.transformSeverityToMatIconFont(data.severity);
+  }
+
+  public ngOnInit(): void {
+    this.subscriptions.add(this.screenMode$.pipe(tap((screenMode) => (this.screenMode = screenMode))).subscribe());
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
   }
 
   public markPageNotificationAsRead() {
