ed6b3d8fb16a729c35a6ca5dc2959acf6fb0cdb8
till_schuetze <till.schuetze@ebp.ch>
2023-11-09 10:52:04 +0100

    GB3-857: start working on mobile for support page

--- src/app/support-page/support-page.component.ts
+++ src/app/support-page/support-page.component.ts
@@ -1,11 +1,33 @@
-import {Component} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Store} from '@ngrx/store';
+import {Subscription, tap} from 'rxjs';
 import {SupportPage} from '../shared/enums/support-page.enum';
+import {ScreenMode} from '../shared/types/screen-size.type';
+import {selectScreenMode} from '../state/app/reducers/app-layout.reducer';
+
+const SUPPORT_PAGE_SUMMARY =
+  'Herzlich Willkommen im neuen GIS-Browser! Wir verstehen, dass die Verwendung eines neuen Tools manchmal überwältigend sein kann, aber keine Sorge, wir sind hier, um Ihnen zu helfen. Unser Ziel ist es, Ihnen eine nahtlose und benutzerfreundliche Erfahrung zu bieten, damit Sie schnell und einfach auf die gewünschten Informationen zugreifen können. In diesem Hilfe- und Support-Bereich finden Sie alle Informationen, die Sie benötigen, um den neuen Kartenviewer zu nutzen.';
 
 @Component({
   selector: 'support-page',
   templateUrl: './support-page.component.html',
   styleUrls: ['./support-page.component.scss'],
 })
-export class SupportPageComponent {
+export class SupportPageComponent implements OnInit, OnDestroy {
   protected readonly supportPageEnum = SupportPage;
+  public heroText = SUPPORT_PAGE_SUMMARY;
+  public screenMode: ScreenMode = 'regular';
+
+  private readonly screenMode$ = this.store.select(selectScreenMode);
+  private readonly subscriptions: Subscription = new Subscription();
+
+  constructor(private readonly store: Store) {}
+
+  public ngOnInit(): void {
+    this.subscriptions.add(this.screenMode$.pipe(tap((screenMode) => (this.screenMode = screenMode))).subscribe());
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
 }
