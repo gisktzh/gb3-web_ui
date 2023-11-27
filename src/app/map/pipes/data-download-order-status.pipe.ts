7b66136c6189c509709b93cb8c387070b3042b37
Sebastian Wendland <Sebastian.Wendland@ebp.ch>
2023-11-27 13:07:15 +0100

    GB3-652: Added `data-download-order-status.pipe.spec` tests.

--- src/app/map/pipes/data-download-order-status.pipe.ts
+++ src/app/map/pipes/data-download-order-status.pipe.ts
@@ -5,32 +5,43 @@ import {OrderStatusJob} from '../../shared/interfaces/geoshop-order-status.inter
   name: 'dataDownloadOrderStatus',
 })
 export class DataDownloadOrderStatusPipe implements PipeTransform {
+  public static readonly ABORTED_STATUS_JOB_TEXT =
+    'Beim Aktualisieren einer Bestellung sind mehrere Fehler aufgetreten und der Prozess wurde abgebrochen.';
+  public static readonly CANCELLED_STATUS_JOB_TEXT = 'Die Bestellung wurde abgebrochen.';
+  public static readonly NEW_STATUS_JOB_TEXT = 'Die Bestellung wird an den Server gesendet.';
+  public static readonly STATUS_JOB_SUBMITTED_TEXT = 'Die Bestellung ist beim Server eingegangen.';
+  public static readonly STATUS_JOB_QUEUED_TEXT = 'Die Bestellung wurde in die Warteschlange aufgenommen.';
+  public static readonly STATUS_JOB_WORKING_TEXT = 'Die Bestellung wird bearbeitet.';
+  public static readonly STATUS_JOB_SUCCESS_TEXT = 'Die Bestellung ist bereit.';
+  public static readonly STATUS_JOB_FAILURE_DEFAULT_TEXT = 'Fehler: Die Bestellung ist fehlgeschlagen.';
+  public static readonly STATUS_JOB_UNKNOWN_TEXT = 'Unbekannter Zustand.';
+
   public transform(orderStatusJob: OrderStatusJob): string {
     if (orderStatusJob.isAborted) {
-      return 'Beim Aktualisieren einer Bestellung sind mehrere Fehler aufgetreten und der Prozess wurde abgebrochen.';
+      return DataDownloadOrderStatusPipe.ABORTED_STATUS_JOB_TEXT;
     }
     if (orderStatusJob.isCancelled) {
-      return 'Die Bestellung wurde abgebrochen.';
+      return DataDownloadOrderStatusPipe.CANCELLED_STATUS_JOB_TEXT;
     }
     if (!orderStatusJob.status) {
-      return 'Die Bestellung wird an den Server gesendet.';
+      return DataDownloadOrderStatusPipe.NEW_STATUS_JOB_TEXT;
     }
     switch (orderStatusJob.status.status.type) {
       case 'submitted':
-        return 'Die Bestellung ist beim Server eingegangen.';
+        return DataDownloadOrderStatusPipe.STATUS_JOB_SUBMITTED_TEXT;
       case 'queued':
-        return 'Die Bestellung wurde in die Warteschlange aufgenommen.';
+        return DataDownloadOrderStatusPipe.STATUS_JOB_QUEUED_TEXT;
       case 'working':
-        return 'Die Bestellung wird bearbeitet.';
+        return DataDownloadOrderStatusPipe.STATUS_JOB_WORKING_TEXT;
       case 'success':
-        return 'Die Bestellung ist bereit.';
+        return DataDownloadOrderStatusPipe.STATUS_JOB_SUCCESS_TEXT;
       case 'failure':
         if (orderStatusJob.status.status.message) {
           return `Fehler: '${orderStatusJob.status.status.message}'`;
         }
-        return 'Fehler: Die Bestellung ist fehlgeschlagen.';
+        return DataDownloadOrderStatusPipe.STATUS_JOB_FAILURE_DEFAULT_TEXT;
       case 'unknown':
-        return 'Unbekannter Zustand.';
+        return DataDownloadOrderStatusPipe.STATUS_JOB_UNKNOWN_TEXT;
     }
   }
 }
