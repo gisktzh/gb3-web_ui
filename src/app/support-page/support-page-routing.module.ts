7071c496bb5ca38fc9c2435795017f48c3ca5434
till_schuetze <till.schuetze@ebp.ch>
2023-12-05 17:07:23 +0100

    GB3-857: add the navigation tabs to the support page. Buttons are missing the rounded edges currently

--- src/app/support-page/support-page-routing.module.ts
+++ src/app/support-page/support-page-routing.module.ts
@@ -1,11 +1,33 @@
 import {NgModule} from '@angular/core';
 import {RouterModule, Routes} from '@angular/router';
 import {SupportPageComponent} from './support-page.component';
+import {FaqComponent} from './components/faq/faq.component';
+import {UsefulLinksComponent} from './components/useful-links/useful-links.component';
+import {ContactComponent} from './components/contact/contact.component';
 
 const routes: Routes = [
   {
     path: '',
     component: SupportPageComponent,
+    children: [
+      {
+        path: 'faq',
+        component: FaqComponent,
+      },
+      {
+        path: 'useful-links',
+        component: UsefulLinksComponent,
+      },
+      {
+        path: 'contact',
+        component: ContactComponent,
+      },
+      {
+        pathMatch: 'full',
+        path: '',
+        redirectTo: 'faq',
+      },
+    ],
   },
 ];
 
--- src/app/support-page/support-page.module.ts
+++ src/app/support-page/support-page.module.ts
@@ -6,9 +6,10 @@ import {FaqComponent} from './components/faq/faq.component';
 import {ContactComponent} from './components/contact/contact.component';
 import {UsefulLinksComponent} from './components/useful-links/useful-links.component';
 import {CommonModule} from '@angular/common';
+import {SupportPageNavigationComponent} from './components/support-page-navigation/support-page-navigation.component';
 
 @NgModule({
-  declarations: [SupportPageComponent, FaqComponent, ContactComponent, UsefulLinksComponent],
+  declarations: [SupportPageComponent, FaqComponent, ContactComponent, UsefulLinksComponent, SupportPageNavigationComponent],
   imports: [CommonModule, SharedModule, SupportPageRoutingModule],
 })
 export class SupportPageModule {}
