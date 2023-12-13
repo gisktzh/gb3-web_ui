18147f66915594ec4562fc28f198179356c04966
till_schuetze <till.schuetze@ebp.ch>
2023-12-13 15:18:22 +0100

    GB3-857 replace strings with values from enum for pages in support page

--- src/app/support-page/components/support-page-navigation/support-page-navigation.component.ts
+++ src/app/support-page/components/support-page-navigation/support-page-navigation.component.ts
@@ -1,8 +1,11 @@
 import {Component} from '@angular/core';
+import {SupportPage} from '../../../shared/enums/support-page.enum';
 
 @Component({
   selector: 'support-page-navigation',
   templateUrl: './support-page-navigation.component.html',
   styleUrls: ['./support-page-navigation.component.scss'],
 })
-export class SupportPageNavigationComponent {}
+export class SupportPageNavigationComponent {
+  protected readonly supportPage = SupportPage;
+}
