178c88fde02727330951449011c7ee13c105adec
Sebastian Wendland <Sebastian.Wendland@ebp.ch>
2023-12-14 15:05:50 +0100

    GB3-348: Added `external-map-item.reducer.spec` and `map-import.reducer.spec`.

---
+++ src/app/state/map/reducers/external-map-item.reducer.spec.ts
@@ -0,0 +1,68 @@
+import {initialState, reducer} from './external-map-item.reducer';
+import {ExternalMapItemState} from '../states/external-map-item.state';
+import {LoadingState} from '../../../shared/types/loading-state.type';
+import {ExternalMapItemActions} from '../actions/external-map-item.actions';
+import {createExternalWmsMapItemMock} from '../../../testing/map-testing/active-map-item-test.utils';
+
+describe('external map item reducer', () => {
+  let existingState: ExternalMapItemState;
+
+  beforeEach(() => {
+    existingState = {
+      loadingState: 'loaded',
+    };
+  });
+
+  describe('an unknown action', () => {
+    it('should return the previous state', () => {
+      const action = {} as never;
+
+      const result = reducer(initialState, action);
+
+      expect(result).toBe(initialState);
+    });
+  });
+
+  describe('loadItem', () => {
+    it('sets the loading state to `loading`; resets everything else', () => {
+      const expectedLoadingState: LoadingState = 'loading';
+
+      const action = ExternalMapItemActions.loadItem({url: 'some url', serviceType: 'wms'});
+      const state = reducer(existingState, action);
+
+      expect(state.loadingState).toBe(expectedLoadingState);
+    });
+  });
+
+  describe('setItem', () => {
+    it('sets the loading state to `loaded`; resets everything else', () => {
+      existingState.loadingState = 'loading';
+      const expectedLoadingState: LoadingState = 'loaded';
+
+      const action = ExternalMapItemActions.setItem({externalMapItem: createExternalWmsMapItemMock('some url', 'some title', [])});
+      const state = reducer(existingState, action);
+
+      expect(state.loadingState).toBe(expectedLoadingState);
+    });
+  });
+
+  describe('setItemError', () => {
+    it('sets the loading state to `error`; resets everything else', () => {
+      const expectedLoadingState: LoadingState = 'error';
+
+      const action = ExternalMapItemActions.setItemError({error: new Error('some error')});
+      const state = reducer(existingState, action);
+
+      expect(state.loadingState).toBe(expectedLoadingState);
+    });
+  });
+
+  describe('clearLoadingState', () => {
+    it('resets everything', () => {
+      const action = ExternalMapItemActions.clearLoadingState();
+      const state = reducer(existingState, action);
+
+      expect(state.loadingState).toBe(initialState.loadingState);
+    });
+  });
+});
--- src/app/state/map/reducers/map-import.reducer.spec.ts
+++ src/app/state/map/reducers/map-import.reducer.spec.ts
@@ -1,6 +1,23 @@
 import {initialState, reducer} from './map-import.reducer';
+import {MapImportState} from '../states/map-import.state';
+import {MapImportActions} from '../actions/map-import.actions';
+import {MapServiceType} from '../../../map/types/map-service.type';
+import {ExternalLayerSelection} from '../../../shared/interfaces/external-layer-selection.interface';
+import {ExternalLayer} from '../../../shared/interfaces/external-layer.interface';
 
 describe('map import reducer', () => {
+  let existingState: MapImportState;
+
+  beforeEach(() => {
+    existingState = {
+      serviceType: 'kml',
+      url: 'the-bestest-url-of-them-all',
+      title: 'title of the year',
+      imageFormat: 'format c:/',
+      layerSelections: [],
+    };
+  });
+
   describe('an unknown action', () => {
     it('should return the previous state', () => {
       const action = {} as never;
@@ -8,6 +25,134 @@ describe('map import reducer', () => {
       const result = reducer(initialState, action);
 
       expect(result).toBe(initialState);
+    });
+  });
+
+  describe('setServiceType', () => {
+    it('sets the service type; resets everything else', () => {
+      const expectedServiceType: MapServiceType = 'wms';
+
+      const action = MapImportActions.setServiceType({serviceType: expectedServiceType});
+      const state = reducer(existingState, action);
+
+      expect(state.serviceType).toBe(expectedServiceType);
+      expect(state.url).toBe(initialState.url);
+      expect(state.title).toBe(initialState.title);
+      expect(state.imageFormat).toBe(initialState.imageFormat);
+      expect(state.layerSelections).toEqual(initialState.layerSelections);
+    });
+  });
+
+  describe('setUrl', () => {
+    it('sets the url; keeps the service type but resets everything else', () => {
+      const expectedUrl = 'new url';
+
+      const action = MapImportActions.setUrl({url: expectedUrl});
+      const state = reducer(existingState, action);
+
+      expect(state.serviceType).toBe(existingState.serviceType);
+      expect(state.url).toBe(expectedUrl);
+      expect(state.title).toBe(initialState.title);
+      expect(state.imageFormat).toBe(initialState.imageFormat);
+      expect(state.layerSelections).toEqual(initialState.layerSelections);
+    });
+  });
+
+  describe('setLayersAndImageFormat', () => {
+    it('sets the layerSelections and the image format; keeps everything else', () => {
+      const layers: ExternalLayer[] = [
+        {type: 'kml', id: 1, title: 'one', visible: true},
+        {type: 'kml', id: 2, title: 'two', visible: false},
+      ];
+
+      const expectedImageFormat = 'image/jpg';
+      const expectedLayerSelections: ExternalLayerSelection[] = [
+        {layer: layers[0], isSelected: false},
+        {layer: layers[1], isSelected: false},
+      ];
+
+      const action = MapImportActions.setLayersAndImageFormat({layers, imageFormat: expectedImageFormat});
+      const state = reducer(existingState, action);
+
+      expect(state.serviceType).toBe(existingState.serviceType);
+      expect(state.url).toBe(existingState.url);
+      expect(state.title).toBe(existingState.title);
+      expect(state.imageFormat).toBe(expectedImageFormat);
+      expect(state.layerSelections).toEqual(expectedLayerSelections);
+    });
+  });
+
+  describe('selectAllLayers', () => {
+    it('sets all layer selections to the given value; keeps everything else', () => {
+      const layerSelections: ExternalLayerSelection[] = [
+        {layer: {type: 'kml', id: 1, title: 'one', visible: true}, isSelected: false},
+        {layer: {type: 'kml', id: 2, title: 'two', visible: false}, isSelected: true},
+      ];
+      existingState.layerSelections = layerSelections;
+      const isSelected = false;
+
+      const expectedLayerSelections: ExternalLayerSelection[] = [
+        {...layerSelections[0], isSelected},
+        {...layerSelections[1], isSelected},
+      ];
+
+      const action = MapImportActions.selectAllLayers({isSelected});
+      const state = reducer(existingState, action);
+
+      expect(state.serviceType).toBe(existingState.serviceType);
+      expect(state.url).toBe(existingState.url);
+      expect(state.title).toBe(existingState.title);
+      expect(state.imageFormat).toBe(existingState.imageFormat);
+      expect(state.layerSelections).toEqual(expectedLayerSelections);
+    });
+  });
+
+  describe('toggleLayerSelection', () => {
+    it('toggles the selection of the given layer; keeps everything else', () => {
+      const layerSelections: ExternalLayerSelection[] = [
+        {layer: {type: 'kml', id: 1, title: 'one', visible: true}, isSelected: false},
+        {layer: {type: 'kml', id: 2, title: 'two', visible: false}, isSelected: true},
+      ];
+      existingState.layerSelections = layerSelections;
+
+      const expectedLayerSelections: ExternalLayerSelection[] = [{...layerSelections[0], isSelected: true}, {...layerSelections[1]}];
+
+      const action = MapImportActions.toggleLayerSelection({layerId: layerSelections[0].layer.id});
+      const state = reducer(existingState, action);
+
+      expect(state.serviceType).toBe(existingState.serviceType);
+      expect(state.url).toBe(existingState.url);
+      expect(state.title).toBe(existingState.title);
+      expect(state.imageFormat).toBe(existingState.imageFormat);
+      expect(state.layerSelections).toEqual(expectedLayerSelections);
+    });
+  });
+
+  describe('setTitle', () => {
+    it('sets the title; keeps everything else', () => {
+      const expectedTitle = 'new title';
+
+      const action = MapImportActions.setTitle({title: expectedTitle});
+      const state = reducer(existingState, action);
+
+      expect(state.serviceType).toBe(existingState.serviceType);
+      expect(state.url).toBe(existingState.url);
+      expect(state.title).toBe(expectedTitle);
+      expect(state.imageFormat).toBe(existingState.imageFormat);
+      expect(state.layerSelections).toEqual(existingState.layerSelections);
+    });
+  });
+
+  describe('clearAll', () => {
+    it('resets everything', () => {
+      const action = MapImportActions.clearAll();
+      const state = reducer(existingState, action);
+
+      expect(state.serviceType).toBe(initialState.serviceType);
+      expect(state.url).toBe(initialState.url);
+      expect(state.title).toBe(initialState.title);
+      expect(state.imageFormat).toBe(initialState.imageFormat);
+      expect(state.layerSelections).toEqual(initialState.layerSelections);
     });
   });
 });
