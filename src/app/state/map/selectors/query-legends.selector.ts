0bf1e523a61353373ee337e4dad34ed021588407
till_schuetze <till.schuetze@ebp.ch>
2023-12-20 17:26:43 +0100

    GB3-841 initial commit to only load the legends of visible layers. Will have to be reverted once the api is ready to deliver the scale info

--- src/app/state/map/selectors/query-legends.selector.ts
+++ src/app/state/map/selectors/query-legends.selector.ts
@@ -3,13 +3,16 @@ import {isActiveMapItemOfType} from '../../../shared/type-guards/active-map-item
 import {Gb2WmsActiveMapItem} from '../../../map/models/implementations/gb2-wms.model';
 import {selectItems} from '../reducers/active-map-item.reducer';
 import {QueryTopic} from '../../../shared/interfaces/query-topic.interface';
+import {selectScale} from '../reducers/map-config.reducer';
 
-export const selectQueryLegends = createSelector(selectItems, (activeMapItems) => {
+export const selectQueryLegends = createSelector(selectItems, selectScale, (activeMapItems, scale) => {
   const queryTopics: QueryTopic[] = activeMapItems
     .filter(isActiveMapItemOfType(Gb2WmsActiveMapItem))
     .filter((activeMapItem) => activeMapItem.visible)
     .map((mapItem) => {
-      const layersToQuery: string[] = mapItem.settings.layers.filter((layer) => layer.visible).map((layer) => layer.layer);
+      const layersToQuery: string[] = mapItem.settings.layers
+        .filter((layer) => layer.visible && layer.minScale < scale && layer.maxScale > scale)
+        .map((layer) => layer.layer);
       return {
         topic: mapItem.settings.mapId,
         layersToQuery: layersToQuery.join(','),
