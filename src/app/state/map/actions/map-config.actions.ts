4338d9440677d76df842f41ad80ee1600f05578f
Sebastian Wendland <Sebastian.Wendland@ebp.ch>
2024-03-27 16:42:30 +0100

    Replaced all instances of `{x: number; y: number}` with the new interface `Coordinates`.

--- src/app/state/map/actions/map-config.actions.ts
+++ src/app/state/map/actions/map-config.actions.ts
@@ -1,6 +1,7 @@
 import {createActionGroup, emptyProps, props} from '@ngrx/store';
 import {ZoomType} from '../../../shared/types/zoom.type';
 import {PointWithSrs} from '../../../shared/interfaces/geojson-types-with-srs.interface';
+import {Coordinates} from '../../../shared/interfaces/coordinate.interface';
 
 export const MapConfigActions = createActionGroup({
   source: 'Map Config',
@@ -22,7 +23,7 @@ export const MapConfigActions = createActionGroup({
     'Change Zoom': props<{zoomType: ZoomType}>(),
     'Set Basemap': props<{activeBasemapId: string}>(),
     'Clear Initial Maps Config': emptyProps(),
-    'Handle Map Click': props<{x: number; y: number}>(), // meta action which effects can hook into that need to deal with map clicks
+    'Handle Map Click': props<Coordinates>(), // meta action which effects can hook into that need to deal with map clicks
     'Handle Map Rotation': props<{rotation: number}>(), // meta action which effects can hook into that need to deal with changes of the map rotation
     'Clear Feature Info Content': emptyProps(), // meta action which effects can hook into that need to clear the feature info
   },
