34377cfba1d949000edfe982de765549006caba0
Lukas Merz <lukemerz@gmail.com>
2024-04-08 13:41:15 +0200

    GB3-1310: POC for print interface

--- src/app/shared/interfaces/print-config.interface.ts
+++ src/app/shared/interfaces/print-config.interface.ts
@@ -1,3 +1,24 @@
+import {DocumentFormat, DpiSetting, FileFormat} from './print-rules.interface';
+
+type ReportOrientation = 'landscape' | 'portrait';
+
+interface DefaultPrintSettings {
+  documentFormat: DocumentFormat;
+  fileFormat: FileFormat;
+  dpiSetting: DpiSetting;
+  orientation: ReportOrientation;
+  legend: boolean;
+}
 export interface PrintConfig {
   scales: number[];
+  pixelSizes: {
+    [K in keyof typeof DocumentFormat]: {
+      [K in ReportOrientation]: {
+        width: number;
+        height: number;
+      };
+    };
+  };
+  defaultPrintValues: DefaultPrintSettings;
+  defaultMapSetPrintValues: DefaultPrintSettings;
 }
