3f9f818ba1daa35af9e57eb7756c9495e7b181e1
Sebastian Wendland <Sebastian.Wendland@ebp.ch>
2024-06-19 15:38:42 +0200

    GB3-1546: Enabled iframe again.

--- src/app/shared/configs/feature-flags.config.ts
+++ src/app/shared/configs/feature-flags.config.ts
@@ -3,5 +3,5 @@ import {FeatureFlags} from '../interfaces/feature-flags.interface';
 export const defaultFeatureFlags: FeatureFlags = {
   oerebExtract: false,
   ownershipInformation: false,
-  iframeShareLink: false,
+  iframeShareLink: true,
 };
