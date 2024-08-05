def172fde0a88b615bf438e2fd2ca1d2acd0118a
Lukas Merz <lukemerz@gmail.com>
2024-08-05 14:33:22 +0200

    GB3-1597: Fix issue with broken font size after loggin in

--- src/app/shared/utils/storage.utils.spec.ts
+++ src/app/shared/utils/storage.utils.spec.ts
@@ -4,9 +4,15 @@ import {TimeExtentUtils} from './time-extent.utils';
 describe('StorageUtils', () => {
   describe('parseJson', () => {
     it(`parses a Json with valid Dates correctly`, () => {
-      const stringToParse: string = '{"date":"1506-01-01T00:00:00.000Z", "number": 2683132, "string": "test"}';
+      const stringToParse: string =
+        '{"date":"1506-01-01T00:00:00.000Z", "number": 2683132, "string": "test", "stringifiedNumberParseableAsDate": "12"}';
 
-      const expectedJsonObject = {date: TimeExtentUtils.parseDefaultUTCDate('1506-01-01T00:00:00.000Z'), number: 2683132, string: 'test'};
+      const expectedJsonObject = {
+        date: TimeExtentUtils.parseDefaultUTCDate('1506-01-01T00:00:00.000Z'),
+        number: 2683132,
+        string: 'test',
+        stringifiedNumberParseableAsDate: '12',
+      };
       expect(StorageUtils.parseJson(stringToParse)).toEqual(expectedJsonObject);
     });
     it(`does not parses an invalid Date-String `, () => {
