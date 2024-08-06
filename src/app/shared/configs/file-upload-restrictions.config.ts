2f905e877fcc8cae39c3585f2331745aa8294feb
till_schuetze <till.schuetze@ebp.ch>
2024-08-06 17:02:43 +0200

    gb3-364 pr fixes

--- src/app/shared/configs/file-upload-restrictions.config.ts
+++ src/app/shared/configs/file-upload-restrictions.config.ts
@@ -1,11 +1,7 @@
-import {Restrictions} from '@uppy/core/lib/Restricter';
+import {Restrictions} from '@uppy/core';
 
 export const FileUploadRestrictionsConfig: Restrictions = {
   maxNumberOfFiles: 1,
   maxFileSize: 10000000, // 10MB
   allowedFileTypes: ['.kml', '.json', '.geojson'],
-  minFileSize: 0,
-  requiredMetaFields: [],
-  minNumberOfFiles: 0,
-  maxTotalFileSize: 10000000, // 10MB
 };
