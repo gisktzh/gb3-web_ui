aad3a99b64d6ecbdfb306760f615e33b6ed560bb
till_schuetze <till.schuetze@ebp.ch>
2024-08-12 10:46:40 +0200

    gb3-1594 add a debounce to the text color edit the prevent lagging when changing colors quickly

--- src/app/map/components/drawing-edit-overlay/drawing-edit/text-edit/text-edit.component.ts
+++ src/app/map/components/drawing-edit-overlay/drawing-edit/text-edit/text-edit.component.ts
@@ -1,19 +1,39 @@
-import {Component, EventEmitter, Input, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {Gb3TextStyle, TextStyleConfiguration} from '../../../../../shared/interfaces/internal-drawing-representation.interface';
 import {MapConstants} from '../../../../../shared/constants/map.constants';
+import {debounceTime, Subject, Subscription} from 'rxjs';
 
 @Component({
   selector: 'text-edit',
   templateUrl: './text-edit.component.html',
   styleUrl: './text-edit.component.scss',
 })
-export class TextEditComponent {
+export class TextEditComponent implements OnInit, OnDestroy {
   @Input() public textStyle!: Gb3TextStyle;
   @Input() public labelText!: string;
 
   @Output() public updateStyleEvent = new EventEmitter<{style: Gb3TextStyle; labelText: string}>();
 
   public maxLength: number = MapConstants.TEXT_DRAWING_MAX_LENGTH;
+
+  private readonly subscriptions = new Subscription();
+  private readonly debouncer = new Subject<{
+    field: keyof TextStyleConfiguration;
+    value: number | string;
+  }>();
+
+  public ngOnInit(): void {
+    this.subscriptions.add(
+      this.debouncer.pipe(debounceTime(10)).subscribe(({field, value}) => {
+        console.log('debounced');
+        this.updateValue(field, value);
+      }),
+    );
+  }
+
+  public ngOnDestroy(): void {
+    this.subscriptions.unsubscribe();
+  }
   public updateValue(field: keyof TextStyleConfiguration, value: number | string) {
     if (field !== 'label') {
       this.textStyle = {
@@ -24,5 +44,9 @@ export class TextEditComponent {
     if (this.labelText) {
       this.updateStyleEvent.emit({style: this.textStyle, labelText: this.labelText});
     }
+  }
+
+  public updatedValueWithDelay(field: keyof TextStyleConfiguration, value: number | string) {
+    this.debouncer.next({field, value});
   }
 }
