d372622c89eacc4011fd6a09ad13489dea0d6636
Lukas Merz <lukemerz@gmail.com>
2024-08-21 16:40:35 +0200

    GB3-1649: Make links accessible in FAQ

--- src/app/shared/pipes/format-content.pipe.ts
+++ src/app/shared/pipes/format-content.pipe.ts
@@ -8,9 +8,15 @@ export class FormatContentPipe implements PipeTransform {
     return this.replaceUrls(value);
   }
 
-  public replaceUrls(value: string): string {
-    // see: https://stackoverflow.com/a/3809435
-    const urlCapturingGroup = /(https?:\/\/(www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*))/gi;
-    return value.replace(urlCapturingGroup, '<a href="$1" target="_blank">$1</a>');
+  public replaceUrls(url: string): string {
+    /**
+     * Based on https://stackoverflow.com/a/3809435 and extended to provide two capture tokens: (1) the protocol and (2) the host, which
+     * allows us to replace links with an accessible label, i.e. there URL without the protocol.
+     */
+    const urlCapturingGroup = /(https?:\/\/)((www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*))/gi;
+    return url.replace(urlCapturingGroup, (match, protocol, host) => {
+      const hostWithoutTrailingSlashes = host.endsWith('/') ? host.slice(0, -1) : host;
+      return `<a href="${match}" target="_blank">${hostWithoutTrailingSlashes}</a>`;
+    });
   }
 }
