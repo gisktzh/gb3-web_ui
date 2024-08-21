8834553dc6369bb08159310d0e518e52c9591d55
Lukas Merz <lukemerz@gmail.com>
2024-08-21 16:55:13 +0200

    GB3-1649: fix sonar cloud mocking my regex skills

--- src/app/shared/pipes/format-content.pipe.ts
+++ src/app/shared/pipes/format-content.pipe.ts
@@ -13,7 +13,7 @@ export class FormatContentPipe implements PipeTransform {
      * Based on https://stackoverflow.com/a/3809435 and extended to provide two capture tokens: (1) the protocol and (2) the host, which
      * allows us to replace links with an accessible label, i.e. there URL without the protocol.
      */
-    const urlCapturingGroup = /(https?:\/\/)((www\.)?[-a-zA-Z0-9@:%._+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b([-a-zA-Z0-9()@:%_+.~#?&//=]*))/gi;
+    const urlCapturingGroup = /(https?:\/\/)((www\.)?[-a-zA-Z0-9@:%._+~#?&//=]{1,256}\.[a-zA-Z0-9()]{1,6}\b[-a-zA-Z0-9()@:%_+.~#?&//=]*)/gi;
     return url.replace(urlCapturingGroup, (match, protocol, host) => {
       const hostWithoutTrailingSlashes = host.endsWith('/') ? host.slice(0, -1) : host;
       return `<a href="${match}" target="_blank">${hostWithoutTrailingSlashes}</a>`;
