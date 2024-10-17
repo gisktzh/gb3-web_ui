fa09fe9bd1a62d95170a713e1d83825d52d4df3f
till_schuetze <till.schuetze@ebp.ch>
2024-10-17 16:23:03 +0200

    label all icon buttons on map page and inside dialogs

--- src/app/map/components/map-tools/measurement-tools/measurement-tools.component.ts
+++ src/app/map/components/map-tools/measurement-tools/measurement-tools.component.ts
@@ -1,12 +1,20 @@
 import {Component} from '@angular/core';
 import {AbstractToolsComponent} from '../abstract-tools/abstract-tools.component';
 
+const TOOLTIP_TEXT = {
+  pointMeasurement: 'Punkt: In Karte klicken um zu wählen.',
+  lineMeasurement: 'Strecke: Mit Doppelklick beenden.',
+  areaMeasurement: 'Fläche: Auf Startpunkt klicken oder Doppelklick um zu beenden.',
+  circleMeasurement: 'Kreis: Mittelpunkt und Radius wählen.',
+  elevationProfileMeasurement: 'Höhenprofil: Mit Doppelklick beenden, um das Profil zu laden.',
+};
 @Component({
   selector: 'measurement-tools',
   templateUrl: './measurement-tools.component.html',
   styleUrls: ['./measurement-tools.component.scss'],
 })
 export class MeasurementToolsComponent extends AbstractToolsComponent {
+  public tooltipText = TOOLTIP_TEXT;
   public togglePointMeasurement() {
     this.toggleTool('measure-point');
   }
