659b443fb29fa5c8f288650e9cc94b9ecbf52ebf
till_schuetze <till.schuetze@ebp.ch>
2024-11-21 14:04:26 +0100

    GB3-1715: Setup Arrow Navigation in Map search results with renderer2

---
+++ src/app/shared/directives/search-result-identifier.directive.spec.ts
@@ -0,0 +1,8 @@
+import {SearchResultIdentifierDirective} from './search-result-identifier.directive';
+
+describe('SearchResultIdentifierDirective', () => {
+  it('should create an instance', () => {
+    const directive = new SearchResultIdentifierDirective();
+    expect(directive).toBeTruthy();
+  });
+});
---
+++ src/app/shared/directives/search-result-identifier.directive.ts
@@ -0,0 +1,24 @@
+import {Directive, ElementRef, EventEmitter, Input, Output} from '@angular/core';
+
+@Directive({
+  selector: '[searchResultIdentifier]',
+  standalone: true,
+})
+export class SearchResultIdentifierDirective {
+  @Input() public isMapResult: boolean = false;
+  @Output() public readonly addResultFromArrowNavigation = new EventEmitter<void>();
+  @Output() public readonly removeResultFromArrowNavigation = new EventEmitter<void>();
+  constructor(public readonly host: ElementRef<HTMLElement>) {}
+
+  public dispatchEventIfMapResult() {
+    if (this.isMapResult) {
+      this.addResultFromArrowNavigation.emit();
+    }
+  }
+
+  public removeSearchResult() {
+    if (this.isMapResult) {
+      this.removeResultFromArrowNavigation.emit();
+    }
+  }
+}
