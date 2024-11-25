09ed205c4a832463814de76ef31825ffc0ba949d
Lukas Merz <lukemerz@gmail.com>
2024-11-25 08:29:17 +0100

    Adjust code snippets in Uppy

--- src/app/shared/configs/file-upload-restrictions.config.ts
+++ src/app/shared/configs/file-upload-restrictions.config.ts
@@ -1,6 +1,6 @@
-import {Restrictions} from '@uppy/core';
+import Restrictions from '@uppy/core/lib/Restricter';
 
-export const FileUploadRestrictionsConfig: Restrictions = {
+export const FileUploadRestrictionsConfig: Partial<Restrictions.Restrictions> = {
   maxNumberOfFiles: 1,
   maxFileSize: 10_000_000, // 10MB
   allowedFileTypes: ['.kml', '.json', '.geojson'],
