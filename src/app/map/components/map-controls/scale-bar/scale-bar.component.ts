514071b5b97eeb8f40cfcbc464af0ff95c795d39
Lukas Merz <lukemerz@gmail.com>
2025-05-12 17:08:32 +0200

    Fix scalebar NgExpressionChangedAfterChecked

--- src/app/map/components/map-controls/scale-bar/scale-bar.component.ts
+++ src/app/map/components/map-controls/scale-bar/scale-bar.component.ts
@@ -1,6 +1,6 @@
 import {AfterViewInit, Component, ElementRef, OnDestroy, ViewChild} from '@angular/core';
 import {Store} from '@ngrx/store';
-import {filter, Subscription, tap} from 'rxjs';
+import {delay, filter, Subscription, tap} from 'rxjs';
 import {NgClass} from '@angular/common';
 import {selectScaleBarConfig} from '../../../../state/map/selectors/scale-bar-config.selector';
 import {ScaleBarConfig} from '../../../../shared/interfaces/scale-bar-config.interface';
@@ -15,15 +15,15 @@ export class ScaleBarComponent implements AfterViewInit, OnDestroy {
   protected scaleBarLabel: string | undefined;
   @ViewChild('scaleBar', {static: true}) private scaleBar!: ElementRef;
   private readonly subscriptions: Subscription = new Subscription();
-  private scaleBarConfig$ = this.store.select(selectScaleBarConfig);
+  private readonly scaleBarConfig$ = this.store.select(selectScaleBarConfig);
 
   constructor(private readonly store: Store) {}
 
   public ngAfterViewInit() {
-    // we use afterViewInit because then we can be sure that #scaleBar is available
     this.subscriptions.add(
       this.scaleBarConfig$
         .pipe(
+          delay(0), // delayed to a separate task run, to avoid NgExpressionChangedAfterChecked
           filter((scaleBarConfig): scaleBarConfig is ScaleBarConfig => scaleBarConfig !== undefined),
           tap(({scaleBarWidthInPx, value, unit}) => {
             this.scaleBar.nativeElement.style.width = `${scaleBarWidthInPx}px`;
