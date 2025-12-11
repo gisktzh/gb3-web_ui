3499b40979e44e41fdf7ef42047bea41dd8cc849
Pascal Thormeier <pascal.thormeier@bd.zh.ch>
2025-12-11 15:15:17 +0000

    AHD-1512: Adjust report download button styling and always expand.

--- src/app/map/components/feature-info-overlay/feature-info-report-download/feature-info-report-download.component.ts
+++ src/app/map/components/feature-info-overlay/feature-info-report-download/feature-info-report-download.component.ts
@@ -1,12 +1,13 @@
 import {Component, Input} from '@angular/core';
 import {MapOverlayListItemComponent} from '../../map-overlay/map-overlay-list-item/map-overlay-list-item.component';
+import {MatButton} from '@angular/material/button';
 import {MatIcon} from '@angular/material/icon';
 
 @Component({
   selector: 'feature-info-report-download',
   templateUrl: './feature-info-report-download.component.html',
   styleUrls: ['./feature-info-report-download.component.scss'],
-  imports: [MapOverlayListItemComponent, MatIcon],
+  imports: [MapOverlayListItemComponent, MatButton, MatIcon],
 })
 export class FeatureInfoReportDownloadComponent {
   @Input() public reportUrl!: string;
