5c725bd48c8c1580b86db781975a2bcbe64c8de4
Pascal Thormeier <pascal.thormeier@bd.zh.ch>
2026-04-07 12:59:16 +0000

    GHUB-0: Fix knip config hints and npm audit fix.

--- knip.config.ts
+++ knip.config.ts
@@ -6,7 +6,6 @@ export default {
     'src/app/testing/mock-modules/**', // All of them mock an existing module
     'src/app/shared/models/gb3-api-generated.interfaces.ts', // Generated, so we don't want to clean that up every time
     'src/app/shared/models/swisstopo-api-generated.interface.ts', // Generated, so we don't want to clean that up every time
-    'src/app/shared/models/grav-cms-generated.interfaces.ts', // Generated, so we don't want to clean that up every time
     'src/app/shared/models/geoshop-api-generated.interface.ts', // Generated, so we don't want to clean that up every time
     // State management should not be included, since all the exports could potentially be used and re-adding them is harder than ignoreing them
     'src/**/*.reducer.ts',
