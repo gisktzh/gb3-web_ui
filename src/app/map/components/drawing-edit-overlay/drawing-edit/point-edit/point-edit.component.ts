e0a9a9dbe1dbfdbbbb4ed90cf38a348f4612e896
Pascal Thormeier <pascal.thormeier@bd.zh.ch>
2026-07-07 14:00:45 +0000

    AHD-2335: Catch incomplete uploaded drawings via linkedSignals.

--- src/app/map/components/drawing-edit-overlay/drawing-edit/point-edit/point-edit.component.ts
+++ src/app/map/components/drawing-edit-overlay/drawing-edit/point-edit/point-edit.component.ts
@@ -1,9 +1,18 @@
-import {Component, model} from '@angular/core';
+import {Component, linkedSignal, model} from '@angular/core';
 import {Gb3PointStyle} from '../../../../../shared/interfaces/internal-drawing-representation.interface';
 import {SliderEditComponent} from '../slider-edit/slider-edit.component';
 import {ColorPickerEditComponent} from '../color-picker-edit/color-picker-edit.component';
 import {MatDivider} from '@angular/material/divider';
 import {form, FormField} from '@angular/forms/signals';
+
+const DEFAULT_DATA = {
+  strokeWidth: 1,
+  strokeOpacity: 1,
+  strokeColor: '#ff0000',
+  fillOpacity: 1,
+  fillColor: '#ff0000',
+  pointRadius: 1,
+};
 
 @Component({
   selector: 'point-edit',
@@ -13,5 +22,12 @@ import {form, FormField} from '@angular/forms/signals';
 })
 export class PointEditComponent {
   public readonly pointStyle = model.required<Gb3PointStyle>();
-  public pointStyleForm = form(this.pointStyle);
+  public readonly pointStyleFormModel = linkedSignal({
+    source: this.pointStyle,
+    computation: (value) => ({
+      ...DEFAULT_DATA,
+      ...value,
+    }),
+  });
+  public pointStyleForm = form(this.pointStyleFormModel);
 }
