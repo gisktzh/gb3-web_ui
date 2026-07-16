bb50f14d3408b9e2021d3a1b5d5bc07b1cab420b
Elias Fessler <elias.fessler@bd.zh.ch>
2026-07-16 11:40:57 +0000

    GHUB-772: Reactivate parcel ownershipInformation

--- src/app/shared/configs/feature-flags.config.ts
+++ src/app/shared/configs/feature-flags.config.ts
@@ -2,7 +2,7 @@ import {FeatureFlags} from '../interfaces/feature-flags.interface';
 
 export const defaultFeatureFlags: FeatureFlags = {
   oerebExtract: false,
-  ownershipInformation: false,
+  ownershipInformation: true,
   iframeShareLink: true,
   koPlaNavItem: false,
 };
