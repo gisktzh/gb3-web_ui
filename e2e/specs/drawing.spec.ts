ccf93b618da2c23709f78b25a47e14c6b59d5086
Pascal Thormeier <pascal.thormeier@bd.zh.ch>
2026-08-12 14:41:32 +0000

    GHUB-823: Slightly adjust timeouts in drawing e2e test.

--- e2e/specs/drawing.spec.ts
+++ e2e/specs/drawing.spec.ts
@@ -36,9 +36,9 @@ test.describe('Drawing', () => {
         for (const [index, value] of sliderInputValues.entries()) {
           const inputField = sliderInputs[index].locator('input');
           await inputField.fill(value);
-          await page.waitForTimeout(500);
+          await page.waitForTimeout(150);
           await expect(sliderInputs[index].locator('.slider-wrapper__header__value')).toContainText(value);
-          await page.waitForTimeout(500);
+          await page.waitForTimeout(150);
         }
       }
 
