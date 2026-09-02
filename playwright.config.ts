d3f3bcbafd0e63153a56f6ef76d7cafe0d9f4451
Pascal Thormeier <pascal.thormeier@bd.zh.ch>
2026-09-02 12:35:30 +0000

    GHUB-0: Half the number of concurrent playwright workers.

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   fullyParallel: false,
   forbidOnly: !!process.env['CI'],
   retries: process.env['CI'] ? 2 : 0,
-  workers: process.env['CI'] ? 4 : 1,
+  workers: process.env['CI'] ? 2 : 1,
   reporter: 'html',
   globalTeardown: require.resolve('./e2e/global.teardown'),
   use: {
